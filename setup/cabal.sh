2dd739b89fc905b942a8d4af1cc85f553fd5839f
Vonfry <vonfry314@icloud.com>
2017-04-09 12:56:15 +0800

    setup cabal package

--- setup/cabal.sh
+++ setup/cabal.sh
@@ -2,6 +2,7 @@ if command -v cabal >/dev/null 2>&1; then
     cabal update
     cabal install happy
     cabal install ghc-mod
+    cabal install optparse-applicative
 else
     echo_sh "ERROR: NO cabal"
     exit
