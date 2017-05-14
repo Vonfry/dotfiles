56b3b5931c4c02d83a83a9d9ab47525a007ba051
Vonfry <vonfry314@icloud.com>
2017-05-14 11:46:07 +0800

    emacs neotree delete node

--- emacs.d/lisp/init-neotree.el
+++ emacs.d/lisp/init-neotree.el
@@ -11,6 +11,7 @@
     (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
     (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
     (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+    (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
     (define-key evil-normal-state-local-map (kbd "r") 'neotree-rename-node)))
 (add-hook 'evil-local-mode-hook
   (lambda ()
