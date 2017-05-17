cfd6037ec794535856ee002772c5c76ef0f62a46
Vonfry <vonfry314@icloud.com>
2017-05-17 12:55:55 +0800

    emacs tags

--- emacs.d/lisp/init-tags.el
+++ emacs.d/lisp/init-tags.el
@@ -1,12 +1,12 @@
 (require-package 'ggtags)
 (defun vonfry/create-ctags-file ()
-  (add-hook 'evil-local-mode-hook
-    (lambda ()
-      (evil-leader/set-key
-        "." 'gtags-find-tag-dwim))) ;; default keybinding is with M- in evil insert mode.
+  (interactive)
   (shell-command "ctags -R --fields=+latinKS --extra=+qf ."))
 (add-hook 'evil-local-mode-hook
-  (lambda () (evil-leader/set-key "r" 'vonfry/create-ctags-file)))
+  (lambda ()
+    (evil-leader/set-key
+      "g." 'gtags-find-tag-dwim    ;; default keybinding is with M- in evil insert mode.
+      "r"  'vonfry/create-ctags-file)))
 
 
 (provide 'init-tags)
