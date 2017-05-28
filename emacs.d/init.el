33e99c308bebe69cdb30e6c44e610263f55bc42a
Vonfry <vonfry314@icloud.com>
2017-05-28 09:57:12 +0800

    emacs load custom-file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -63,3 +63,5 @@ re-downloaded in order to locate PACKAGE."
 (provide 'init)
 
 (setq custom-file (expand-file-name  "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
