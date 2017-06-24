4617d4f2a6b5da0ffacdfa31cf84cc46b8ab7452
Vonfry <vonfry314@icloud.com>
2017-06-24 12:00:54 +0800

    gem add

--- irbrc
+++ irbrc
@@ -1,2 +1,5 @@
+require 'irb/completion'
+IRB.conf[:AUTO_INDENT] = true
+
 require 'irbtools'
 require 'irbtools/more'
--- setup/gem.sh
+++ setup/gem.sh
@@ -1,6 +1,6 @@
 if command -v gem > /dev/null 2>&1; then
     echo_sh "** setup gem"
-    gem install -g $script_dir gem.txt
+    gem install -g $script_dir/gem.txt
 else
     echo_sh "ERROR: NO gem"
     exit
