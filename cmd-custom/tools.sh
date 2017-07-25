1983e0ab314402fd30cec835e2154364dd437465
Vonfry <vonfry314@icloud.com>
2017-07-25 10:25:46 +0800

    zshrc tools for stack

--- cmd-custom/tools.sh
+++ cmd-custom/tools.sh
@@ -1,3 +1,7 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 eval "$(thefuck --alias)"
+
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+eval "$(stack --bash-completion-script stack)"
