3c0ff0db58b69f971e6d0b1d1a93c3499c3ebe0f
Vonfry <vonfry314@icloud.com>
2017-07-28 11:52:25 +0800

    docs(core): name rule

---
+++ emacs.d/core/core.el
@@ -0,0 +1,17 @@
+;;; core.el --- the heart of the beast -*- lexical-binding: t; -*-
+
+;;; Naming conventions:
+;;
+;;   vonfry-...   public variables or non-interactive functions
+;;   vonfry--...  private anything (non-interactive), not safe for direct use
+;;   vonfry/...   an interactive function; safe for M-x or keybinding
+;;   vonfry:...   an evil operator, motion or command
+;;   vonfry|...   hook function
+;;   vonfry*...   advising functions
+;;   ...!         a macro or function that configures DOOM
+;;   %...         functions used for in-snippet logic
+;;   +...         Any of the above but part of a module, e.g. `+emacs-lisp|init-hook'
+;;
+;; Autoloaded functions are in core/autoload/*.el and modules/*/*/autoload.el or
+;; modules/*/*/autoload/*.el.
+
