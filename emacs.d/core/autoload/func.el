94a86b13abb96d0025209bebe1d1318beef06570
Vonfry <vonfry314@icloud.com>
2017-08-05 12:09:52 +0800

    feat(autoload): insert-currect-date

---
+++ emacs.d/core/autoload/func.el
@@ -0,0 +1,10 @@
+;;; autoload - func -*- lexical-binding: -*-
+
+(vonfry-packages! 'exec-path-from-shell)
+(vonfry-use-package! 'exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize)
+  (defun vonfry/insert-current-date ()
+    (interactive)
+    (insert (format-time-string "%Y-%m-%d %H:%M:%S +0800"))))
+
