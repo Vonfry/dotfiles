79ea351a0fb88f9eb74e0d4d5860a6fade7f932d
Vonfry <vonfry314@icloud.com>
2017-08-05 10:54:29 +0800

    refactor(undotree): dimish

--- emacs.d/modules/visual/undotree/config.el
+++ emacs.d/modules/visual/undotree/config.el
@@ -4,13 +4,12 @@
   :init
   (defcustom +undotree-evil-nmap-prefix vonfry-keybind-evil-leader :group 'vonfry-module)
   (defcustom +undotree-history-directory (concat "undotree/" vonfry-local-dir) :group 'vonfry-module)
+  :diminish undo-tree-mode
   :config
   (global-undo-tree-mode)
   (custom-set-variables
     '(undo-tree-auto-save-history t)
     '(undo-tree-history-directory-alist `((".*" ,+undotree-history-directory))`))
-  (with-eval-after-load 'diminish
-    (diminish 'undo-tree-mode))
   :general
   (nmap :prefix +undotree-evil-nmap-prefix
         "u" 'undo-tree-visualize)
