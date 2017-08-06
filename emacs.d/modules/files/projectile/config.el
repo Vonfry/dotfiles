4ea93b0acdf982c685c7ad6d2a677bfa4104304d
Vonfry <vonfry314@icloud.com>
2017-08-06 13:07:49 +0800

    feat(emacs): projectlie add other plugins

--- emacs.d/modules/files/projectile/config.el
+++ emacs.d/modules/files/projectile/config.el
@@ -14,3 +14,20 @@
     (nmap :prefix vonfry-keybind-evil-leader
       vonfry-keybind-evil-ctrlp  'helm-projectile-find-file
       vonfry-keybind-evil-search 'helm-projectile-ag))
+
+(vonfry|use-package! org-projectile
+  :bind (("C-c n p" . org-projectile-project-todo-completing-read)
+         ("C-c c" . org-capture))
+  :config
+  (setq org-projectile-projects-file
+        (expand-file-name "org/todo-project.org" vonfry-local-dir))
+  (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
+  (push (org-projectile-project-todo-entry) org-capture-templates))
+
+(vonfry|use-package! org-projectile
+  :config
+  (add-hook 'ibuffer-hook
+    (lambda ()
+      (ibuffer-projectile-set-filter-groups)
+      (unless (eq ibuffer-sorting-mode 'alphabetic)
+        (ibuffer-do-sort-by-alphabetic)))))
