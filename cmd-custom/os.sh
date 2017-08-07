7811c312a8205344e2938a372d976166faf3ae4d
Vonfry <vonfry314@icloud.com>
2017-08-07 20:37:31 +0800

    feat(macos): os with llvm

--- cmd-custom/os.sh
+++ cmd-custom/os.sh
@@ -2,6 +2,13 @@
 
 case "$(uname)" in
     "Darwin")
+        export PATH="/usr/local/opt/llvm/bin:$PATH"
+        alias llvm-help="
+        To use the bundled libc++ please add the following LDFLAGS:
+        LDFLAGS=\"-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib\"
+        compile with configure:
+        LDFLAGS:  -L/usr/local/opt/llvm/lib
+        CPPFLAGS: -I/usr/local/opt/llvm/include"
         ;;
     "Linux")
         source /etc/os-release
