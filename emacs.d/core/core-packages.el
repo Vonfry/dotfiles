ac65534968906a775f7b42a04d322654e97b2794
Vonfry <vonfry314@icloud.com>
2017-08-09 19:50:42 +0800

    fix(emacs): update auto

--- emacs.d/core/core-packages.el
+++ emacs.d/core/core-packages.el
@@ -55,9 +55,9 @@ is undefined(It always is loaded by alpha order)."
   '(package-user-dir          vonfry-elpa-dir)
   '(quelpa-dir                vonfry-pkg-manager-dir)
   '(quelpa-stable-p           nil)
-  '(quelpa-checkout-melpa-p   nil)
-  '(quelpa-update-melpa-p     nil)
-  '(quelpa-upgrade-p          nil)
+  '(quelpa-checkout-melpa-p   t)
+  '(quelpa-update-melpa-p     t)
+  '(quelpa-upgrade-p          t)
   '(use-package-always-ensure nil))
 
 (require 'package)
