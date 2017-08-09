47ca5c8c9e1d03ff279916159aa7d646c02768a2
Vonfry <vonfry314@icloud.com>
2017-08-09 18:04:35 +0800

    fix(emacs): auctex without quelpa

--- emacs.d/modules/lang/latex/packages.el
+++ emacs.d/modules/lang/latex/packages.el
@@ -1,7 +1,15 @@
 ;;; latex packages -*- lexical-binding: t -*-
 
+(vonfry|use-package! auctex
+  ; This packages have some problem with package-build.
+  :defer t
+  :ensure t)
+
+;; (vonfry|packages!
+;;   (auctex :fetcher github :repo "emacsmirror/auctex") :upgrade nil)
+
+
 (vonfry|packages!
-  (auctex :fetcher github :repo "emacsmirror/auctex")
   auctex-latexmk
   company-auctex
   company-math)
