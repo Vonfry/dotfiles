24f0c5d05519f835700aa9e0bfdc97e6d0143de4
Vonfry <vonfry314@icloud.com>
2017-08-16 10:19:14 +0800

    feat(statistics): add some statistics dependency

--- setup/cabal.sh
+++ setup/cabal.sh
@@ -1,6 +1,6 @@
 if command -v cabal >/dev/null 2>&1; then
     cabal update
-    cat $script_dir/cabal.txt | xargs -t -n1 npm install -g
+    cat $script_dir/cabal.txt | xargs -t -n1 cabal install
 else
     echo_sh "ERROR: NO cabal"
     exit
