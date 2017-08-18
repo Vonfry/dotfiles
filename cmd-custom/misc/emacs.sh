d3aace35be1d9e1cf6d8fdfb39effb34d52bc79b
Vonfry <vonfry314@icloud.com>
2017-08-18 11:28:36 +0800

    feat(emacs): alias

--- cmd-custom/misc/emacs.sh
+++ cmd-custom/misc/emacs.sh
@@ -2,8 +2,9 @@ export ALTERNATE_EDITOR=""
 
 export EMACSCLIENT_OPT="-n"
 
+alias ecd="emacs --daemon"
+alias ecq="emacsclient -q -t -e \"(kill-emacs)\""
 alias ecc="emacsclient $EMACSCLIENT_OPT -c"
 alias ect="emacsclient -t"
 alias secc="sudo emacsclient $EMACSCLIENT_OPT -c"
 alias sect="sudo emacsclient -t"
-alias emacsc="emacsclient $EMACSCLIENT_OPT -c -a emacs"
