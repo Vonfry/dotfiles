43cfa3b91c58f914525e6ea53ad0ad90bc6fd2be
Vonfry <vonfry314@icloud.com>
2017-08-31 17:22:06 +0800

    feat(ssh): add sshd for macos
    this is not good for macos

---
+++ cmd-custom/misc/ssh.sh
@@ -0,0 +1,14 @@
+function mk-ssh-keygen()
+{
+    sudo ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
+    sudo ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+    sudo ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
+    sudo ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
+}
+
+if [ $(uname) = "Drawin" ]; then
+    function start-sshd()
+    {
+        sudo /usr/sbin/sshd
+    }
+fi
