9904460e85700b5962509caec33114975974d967
Vonfry <vonfry314@icloud.com>
2017-09-27 13:13:58 +0800

    feat(macos): caskroom/cask/intel-power-gadget

--- setup/macos.sh
+++ setup/macos.sh
@@ -86,6 +86,7 @@ sudo ln -f -s $script_dir/etc/motd /etc/motd
 brew cask install iina # video
 brew install axel # http download
 brew cask install deluge # torrent
+brew cask install intel-power-gadget # cpu monitor
 
 # quicklook
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
