15e79a56975d657072609116b3d653ccc91693bf
Vonfry <vonfry314@icloud.com>
2017-10-02 13:29:35 +0800

    feat(setup): add git with gpg

--- setup/git.sh
+++ setup/git.sh
@@ -14,6 +14,11 @@ echo_node "--- input git global github user "
 git config --global user.email $git_global_user
 git config --global user.username $git_global_email
 curl -L -s https://www.gitignore.io/api/vim,tags,macos,linux,emacs,windows > ~/.gitignore_global
+echo_note "--- input gpg keys([key]/n):"
+read git_global_gpg
+if [ $git_global_gpg != "n" ]; then
+    git config --global user.signingkey $git_global_gpg
+fi
 
 git config --global core.excludesfile ~/.gitignore_global
 git config --global commit.tempalet ~/.gitcommit_global
@@ -22,3 +27,4 @@ git config --global core.editor vim # use all with vim, git flow has a bug with 
 
 unset git_global_user
 unset git_global_email
+unset git_global_gpg
