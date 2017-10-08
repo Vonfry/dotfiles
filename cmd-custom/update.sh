9d8da1f60fb950daa7201b8af5ea4cbfd82da75b
Vonfry <vonfry314@gmail.com>
2017-10-08 12:20:47 +0800

    feat(update): pip with --user

--- cmd-custom/update.sh
+++ cmd-custom/update.sh
@@ -63,11 +63,21 @@ function vonfry-update()
     hoogle generate
     echo -e "\n${ECHO_SYM}* ${ECHO_MSG}python${ECHO_RST}\n"
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}pip3${ECHO_RST}\n"
-    pip3 install --upgrade pip setuptools wheel
-    pip3 install -U -r ~/.pip3.txt
+    if [ $(uname) = Darwin ]; then
+        pip3 install --upgrade pip setuptools wheel
+        pip3 install -U -r ~/.pip3.txt
+    else
+        pip3 install --user --upgrade pip setuptools wheel
+        pip3 install --user -U -r ~/.pip3.txt
+    fi
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}pip2${ECHO_RST}\n"
-    pip2 install --upgrade pip setuptools wheel
-    pip2 install -U -r ~/.pip2.txt
+    if [ $(uname) = Darwin ]; then
+        pip2 install --upgrade pip setuptools wheel
+        pip2 install -U -r ~/.pip2.txt
+    else
+        pip2 install --user --upgrade pip setuptools wheel
+        pip2 install --user -U -r ~/.pip2.txt
+    fi
     echo -e "\n${ECHO_SYM}* ${ECHO_MSG}ruby${ECHO_RST}\n"
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}gem${ECHO_RST}\n"
     gem update && gem update --system
