15a5bac3cff32d1ab96d477ef67a850d918c1112
Vonfry <vonfry314@gmail.com>
2017-11-05 14:17:52 +0800

    feat(setup): change hs setup ln

--- setup/haskell.sh
+++ setup/haskell.sh
@@ -1,7 +1,5 @@
 echo_info "** setup haskell"
 
-ln -f -s $script_dir/cabal.txt ~/.cabal.txt
-
 mkdir ~/.stack 2> /dev/null
 ln -f -s $script_dir/stack/* ~/.stack/
 
@@ -15,9 +13,11 @@ if command -v cabal >/dev/null 2>&1; then
     echo_info "*** If you want to use a mirror, please set it by yourself."
     if [ -f $script_dir/cabal.txt ]; then
         cat $script_dir/cabal.txt | xargs -t -n1 cabal install
+        ln -f -s $script_dir/cabal.txt ~/.cabal.txt
     fi
     if [ -f $script_dir/stack.txt ]; then
         cat $script_dir/stack.txt | xargs -t -n1 stack install
+        ln -f -s $script_dir/stack.txt ~/.stack.txt
     fi
 else
     echo_warn "!!! ERROR: NO cabal"
