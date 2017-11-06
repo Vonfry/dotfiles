34cd85ddcc55427745518497dd09f2fe961e43c1
Vonfry <vonfry314@gmail.com>
2017-11-06 16:13:28 +0800

    fix(emacs): disable ggtags by default
    I think I don't need it any longer

--- emacs.d/modules/navigation/ggtags/config.el
+++ emacs.d/modules/navigation/ggtags/config.el
@@ -1,11 +1,13 @@
 ;;; ggtags config -*- lexical-binding: t -*-
 
 (use-package! ggtags
+  :disabled
   :config
   (add-hook 'prog-mode-hook 'ggtags-mode)
   (setq-local imenu-create-index-function #'ggtags-build-imenu-index))
 
 (use-package! helm-gtags
+  :after ggtags
   :init
   (defcustom +helm-gtags-nmap-prefix
     (concat vonfry-keybind-evil-leader vonfry-keybind-evil-gtags)
