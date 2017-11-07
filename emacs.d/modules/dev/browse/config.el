8f36681d8980c8fdc810a9fc18ee4ed598bdc9f9
Vonfry <vonfry314@gmail.com>
2017-11-07 20:18:20 +0800

    feat(emacs): speedbar

--- emacs.d/modules/dev/browse/config.el
+++ emacs.d/modules/dev/browse/config.el
@@ -1,3 +1,15 @@
 ;;; browse config -*- lexical-binding: t -*-
 
-(use-package! ecb)
+(use-package! ecb
+  :init
+  (custom-set-variables
+   '(ecb-auto-expand-directory-tree "best")
+   '(ecb-auto-update-methods-after-save t)))
+
+(use-package! sr-speedbar
+  :config
+  (custom-set-variables
+   '(sr-speedbar-auto-refresh t))
+  :general
+  (nmap :prefix vonfry-keybind-evil-leader
+    vonfry-keybind-evil-speedbar 'sr-speedbar-toggle))
