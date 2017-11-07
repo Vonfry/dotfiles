294b4a1f735ebe2796694124e7735e1df74cf53b
Vonfry <vonfry314@gmail.com>
2017-11-07 20:36:07 +0800

    feat(emacs): projectile-speedbar

--- emacs.d/modules/dev/browse/config.el
+++ emacs.d/modules/dev/browse/config.el
@@ -6,10 +6,16 @@
    '(ecb-auto-expand-directory-tree "best")
    '(ecb-auto-update-methods-after-save t)))
 
+(use-package! speedbar)
+
 (use-package! sr-speedbar
+  :after speedbar
   :config
   (custom-set-variables
-   '(sr-speedbar-auto-refresh t))
+   '(sr-speedbar-auto-refresh t)))
+
+(use-package! projectile-speedbar
+  :after sr-speedbar
   :general
   (nmap :prefix vonfry-keybind-evil-leader
     vonfry-keybind-evil-speedbar 'sr-speedbar-toggle))
--- emacs.d/modules/dev/browse/packages.el
+++ emacs.d/modules/dev/browse/packages.el
@@ -2,3 +2,4 @@
 
 (package! ecb)
 (package! sr-speedbar)
+(package! projectile-speedbar)
