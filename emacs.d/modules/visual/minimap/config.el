2f02866dc058732a91dfae2ab5388695a9682bb7
Vonfry <vonfry314@gmail.com>
2017-11-13 14:23:19 +0800

    fix(emacs): remove scroll because its not useful

--- emacs.d/modules/visual/minimap/config.el
+++ emacs.d/modules/visual/minimap/config.el
@@ -3,7 +3,6 @@
 (use-package! sublimity
   :config
   (use-package! sublimity-map)
-  (use-package! sublimity-scroll)
   (custom-set-variables
     '(sublimity-map-set-delay nil))
   (sublimity-mode 1))
