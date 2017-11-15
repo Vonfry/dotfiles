6fdce818db4d73b8fab5ae5495a396fc16809ee3
Vonfry <vonfry314@gmail.com>
2017-11-15 11:37:23 +0800

    fix(zsh): fix the logic for last comment

--- cmd-custom/misc/keychain.sh
+++ cmd-custom/misc/keychain.sh
@@ -1,4 +1,4 @@
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] || [ -n "$SSH_CONNECTION" ]; then
+if [[ ! ((-n "$SSH_CLIENT") && (-n "$SSH_TTY") && (-n "$SSH_CONNECTION")) ]]; then
     keychain --agents "ssh,gpg" > /dev/null 2>&1
     source ~/.keychain/$(hostname)-sh > /dev/null 2>&1
     source ~/.keychain/$(hostname)-sh-gpg > /dev/null 2>&1
