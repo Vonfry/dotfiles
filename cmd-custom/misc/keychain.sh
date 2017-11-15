da7789e06d645d3639451a38a05c23a4568b9bbc
Vonfry <vonfry314@gmail.com>
2017-11-15 11:23:03 +0800

    fix(zsh): only use keychain when it isn't a ssh client

--- cmd-custom/misc/keychain.sh
+++ cmd-custom/misc/keychain.sh
@@ -1,6 +1,8 @@
-keychain --agents "ssh,gpg" > /dev/null 2>&1
-source ~/.keychain/$(hostname)-sh > /dev/null 2>&1
-source ~/.keychain/$(hostname)-sh-gpg > /dev/null 2>&1
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] || [ -n "$SSH_CONNECTION" ]; then
+    keychain --agents "ssh,gpg" > /dev/null 2>&1
+    source ~/.keychain/$(hostname)-sh > /dev/null 2>&1
+    source ~/.keychain/$(hostname)-sh-gpg > /dev/null 2>&1
+fi
 
 if [ $(uname) = Darwin ]; then
     ssh-add -K > /dev/null 2>&1
