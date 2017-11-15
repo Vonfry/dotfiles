2f06d58280c1641f878eb614d92a37dcc909adba
Vonfry <vonfry314@gmail.com>
2017-11-15 11:40:11 +0800

    docs(zsh): doc

--- cmd-custom/misc/keychain.sh
+++ cmd-custom/misc/keychain.sh
@@ -1,4 +1,5 @@
 if [[ ! ((-n "$SSH_CLIENT") && (-n "$SSH_TTY") && (-n "$SSH_CONNECTION")) ]]; then
+    # Use ssh-forward instead this. which is more safe.
     keychain --agents "ssh,gpg" > /dev/null 2>&1
     source ~/.keychain/$(hostname)-sh > /dev/null 2>&1
     source ~/.keychain/$(hostname)-sh-gpg > /dev/null 2>&1
