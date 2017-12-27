26773a7a580b1d192ab3f453d1050144f695b2ec
Vonfry <vonfry314@gmail.com>
2017-12-27 20:26:28 +0800

    feat(zshrc & emacs): zshrc plugins & emacs config
    zshrc add rails & unalias & emacs add nov.el config

--- emacs.d/modules/misc/novel/config.el
+++ emacs.d/modules/misc/novel/config.el
@@ -1,4 +1,7 @@
 ;;; novel config -*- lexical-binding: t -*-
 
 (use-package! nov
-  :mode (("\\.epub\\'" . nov-mode)))
+  :mode (("\\.epub\\'" . nov-mode))
+  :config
+  (custom-set-variables
+    '(nov-save-place-file (expand-file-name "nov-places" vonfry-local-dir))))
