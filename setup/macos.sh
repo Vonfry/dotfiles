7a2c61b579d947dc6a8582227fc9b83efe709232
Vonfry <vonfry314@gmail.com>
2018-03-09 12:34:20 +0800

    fix(macos): use reinstall instead install
    To make sure the install flag

--- setup/macos.sh
+++ setup/macos.sh
@@ -17,78 +17,78 @@ brew install caskroom/cask/brew-cask
 
 # shell
 brew tap beeftornado/rmtree # rm pkg with deps
-brew install gnu-sed gnu-tar unzip unrar
+brew reinstall gnu-sed gnu-tar unzip unrar
 brew cask install gpg-suite
-brew install git --with-gettext --with-openssl --with-curl --with-blk-sha1
-brew install zsh wget git-flow tig
-brew install fzf && /usr/local/opt/fzf/install
-brew install thefuck
-brew install autojump
-brew install googler
-# brew install htop
-brew install lolcat                        # colorful print
-brew install cloc                          # code statistics
-brew install tree uchardet                 # file browser
-brew install colordiff
-brew install fortune cmatrix figlet        # 诗词，代码雨，ascii-art
-brew install handbrake                     # video transcoder
-brew install parallel
+brew reinstall git --with-gettext --with-openssl --with-curl --with-blk-sha1
+brew reinstall zsh wget git-flow tig
+brew reinstall fzf && /usr/local/opt/fzf/install
+brew reinstall thefuck
+brew reinstall autojump
+brew reinstall googler
+# brew reinstall htop
+brew reinstall lolcat                        # colorful print
+brew reinstall cloc                          # code statistics
+brew reinstall tree uchardet                 # file browser
+brew reinstall colordiff
+brew reinstall fortune cmatrix figlet        # 诗词，代码雨，ascii-art
+brew reinstall handbrake                     # video transcoder
+brew reinstall parallel
 
 # C & Cpp libs
-brew install gcc --with-nls
-brew install llvm --with-lldb --with-graphviz --with-toolchain # This is a dependence for other app(i.e. rtags), and use it insteaded of system llvm(xcode).
-brew install scons # a cross build tools
-brew install gdb --with-all-targets --with-python
-brew install cmake --with-completion
-brew install ninja
-brew install boost zlib libffi
-brew install boost-python3
+brew reinstall gcc --with-nls
+brew reinstall llvm --with-lldb --with-graphviz --with-toolchain # This is a dependence for other app(i.e. rtags), and use it insteaded of system llvm(xcode).
+brew reinstall scons # a cross build tools
+brew reinstall gdb --with-all-targets --with-python
+brew reinstall cmake --with-completion
+brew reinstall ninja
+brew reinstall boost zlib libffi
+brew reinstall boost-python3
 
 # C & Cpp developing tools
-brew tap universal-ctags/universal-ctags && brew install --HEAD universal-ctags
-brew install global --with-ctags --with-pygments --with-sqlite3
-brew install rtags --without-emacs
-brew install cscope
-brew install doxygen --with-doxywizard --with-graphviz
-brew install cflow
+brew tap universal-ctags/universal-ctags && brew reinstall --HEAD universal-ctags
+brew reinstall global --with-ctags --with-pygments --with-sqlite3
+brew reinstall rtags --without-emacs
+brew reinstall cscope
+brew reinstall doxygen --with-doxywizard --with-graphviz
+brew reinstall cflow
 
 # code search
-brew install ack the_silver_searcher awk gawk
+brew reinstall ack the_silver_searcher awk gawk
 
 # lang
-brew install ghc
-brew install cabal-install haskell-stack
-brew install ruby
-brew install rbenv
-brew install clisp sbcl
-brew install ocaml opam
-brew install python3
-brew install python
-brew install go node
-brew install sqlite mysql postgresql redis # database
-brew install poppler                       # pdf generator
-brew install pandoc                        # markdown
+brew reinstall ghc
+brew reinstall cabal-install haskell-stack
+brew reinstall ruby
+brew reinstall rbenv
+brew reinstall clisp sbcl
+brew reinstall ocaml opam
+brew reinstall python3
+brew reinstall python
+brew reinstall go node
+brew reinstall sqlite mysql postgresql redis # database
+brew reinstall poppler                       # pdf generator
+brew reinstall pandoc                        # markdown
 brew cask install mactex                   # providing tex basic tools
 brew cask install texpad                   # a tex ide
 brew cask install java
-brew install ispell                        # emacs flyspell dependency
-brew install shellcheck
+brew reinstall ispell                        # emacs flyspell dependency
+brew reinstall shellcheck
 
 # system tool
-brew install wireshark --with-nghttp2 --iwth-headers --with-libssh --with-qt # web package catch
+brew reinstall wireshark --with-nghttp2 --iwth-headers --with-libssh --with-qt # web package catch
 brew cask install docker
 brew cask install amethyst # a tool like xmonad
-brew install graphviz --with-app
+brew reinstall graphviz --with-app
 
 # chat
 brew cask install gitter
-brew install weechat
+brew reinstall weechat
 
 # editor
-brew install editorconfig
-brew install macvim --with-lua --with-override-system-vim --with-luajit
-brew install neovim
-brew install emacs --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-dbus
+brew reinstall editorconfig
+brew reinstall macvim --with-lua --with-override-system-vim --with-luajit
+brew reinstall neovim
+brew reinstall emacs --with-cocoa --with-gnutls --with-imagemagick@6 --with-librsvg --with-mailutils --with-dbus
 # TODO change imagemagick@6 to other version
 # TODO use emacs-port instead of gnu-emacs until it supports with multi-tty
 # brew tap railwaycat/emacsmacport
@@ -97,7 +97,7 @@ sudo ln -f -s $script_dir/etc/motd /etc/motd
 
 # life
 brew cask install iina # video
-brew install axel # http download
+brew reinstall axel # http download
 brew cask install deluge # torrent
 brew cask install intel-power-gadget # cpu monitor
 brew cask install sigil # epub editor
