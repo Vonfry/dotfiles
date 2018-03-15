f26357dc119ea0b26db034481d9f3cb60d16d0ae
Vonfry <vonfry314@gmail.com>
2018-03-15 15:15:52 +0800

    feat(emacs): flycheck

--- emacs.d/modules/syntax-check/flycheck/config.el
+++ emacs.d/modules/syntax-check/flycheck/config.el
@@ -3,7 +3,9 @@
 (use-package! flycheck
   :config
   (add-hook 'after-init-hook 'global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (custom-set-varibles
+    '(flycheck-check-syntax-automatically '(save mode-enabled))
+    '(flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
   :general
   (nmap :prefix vonfry-keybind-evil-leader
         vonfry-keybind-evil-check 'flycheck-list-errors))
