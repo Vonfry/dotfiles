f0036f8b2a90dd1c22571612a095118776f64e41
Vonfry <vonfry314@gmail.com>
2018-03-19 17:50:07 +0800

    fix(emacs): hl keybind
    add leader

--- emacs.d/modules/visual/todo/config.el
+++ emacs.d/modules/visual/todo/config.el
@@ -3,7 +3,7 @@
 (use-package! hl-todo
   :init
   (defcustom +hl-todo-prefix-keybind
-    (concat vonfry-keybind-evil-highlight "t")
+    (concat vonfry-keybind-evil-leader vonfry-keybind-evil-highlight "t")
     "The prefix keybind for evil normal mode to use hl-todo."
     :type 'string
     :group 'vonfry-module)
