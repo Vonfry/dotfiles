5966554db2b32585b5011351921425bd0164a991
Vonfry <vonfry314@gmail.com>
2018-03-21 20:06:58 +0800

    fix(R): sudo
    in linux

--- setup/R.sh
+++ setup/R.sh
@@ -3,7 +3,11 @@ ln -f -s $script_dir/R.txt ~/.R.txt
 
 if command -v R >/dev/null 2>&1; then
     echo_info "*** setup R"
-    cat $script_dir/R.txt | xargs -n1 -I "{}"  R --slave --quiet -e "install.packages('{}')"
+    if [ $(uname) = Darwin ]; then
+        cat $script_dir/R.txt | xargs -n1 -I "{}"  R --slave --quiet -e "install.packages('{}')"
+    else
+        cat $script_dir/R.txt | xargs -n1 -I "{}"  sudo R --slave --quiet -e "install.packages('{}')"
+    fi
 else
     echo_warn "!!! ERROR: NO R"
     exit 1
