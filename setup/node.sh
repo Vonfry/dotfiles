388aab38d2152e931ce0e36c495411712fa62c2e
Vonfry <vonfry314@gmail.com>
2018-03-25 08:04:42 +0800

    fix(npm): update on setup

--- setup/node.sh
+++ setup/node.sh
@@ -5,8 +5,10 @@ ln -f -s $script_dir/npm.txt ~/.npm.txt
 if command -v npm > /dev/null 2>&1; then
     echo_info "*** setup nmp"
     if [ $(uname) = Darwin ]; then
+        npm update -g
         cat $script_dir/npm.txt | xargs -t -n1 npm install -g
     else
+        sudo npm update -g
         cat $script_dir/npm.txt | xargs -t -n1 sudo npm install -g
     fi
 else
