44a557ad023082bcbd35d708849050ee5126f73e
Vonfry <vonfry314@gmail.com>
2018-04-21 19:23:26 +0800

    fix(setup): fish

--- setup/43fish.sh
+++ setup/43fish.sh
@@ -1,11 +1,11 @@
 ln -s -f $script_dir/config/fish/config ~/.config/fish/config
 
-echo "set CMD_CUSTOM_DIR $HOME/.cmd-custom"             >> $script_dir/cmd-custom/defvar.fish
-echo "set CMD_CUSTOM_DIR_LOCAL $HOME/.cmd-custom/local" >> $script_dir/cmd-custom/defvar.fish
-echo "set DOTFILES_SOURCE_DIR $script_dir"              >> $script_dir/cmd-custom/defvar.fish
-echo "set DOTFILES_DIR $HOME/dotfiles"                  >> $script_dir/cmd-custom/defvar.fish
-echo "set DOTFILES_PKGS_DIR $script_dir/config/pkgs"    >> $script_dir/cmd-custom/defvar.fish
-echo "set DROPBOX_DIR $dropbox_dir"                     >> $script_dir/cmd-custom/defvar.fish
+echo "set -x CMD_CUSTOM_DIR $HOME/.cmd-custom"             >> $script_dir/cmd-custom/defvar.fish
+echo "set -x CMD_CUSTOM_DIR_LOCAL $HOME/.cmd-custom/local" >> $script_dir/cmd-custom/defvar.fish
+echo "set -x DOTFILES_SOURCE_DIR $script_dir"              >> $script_dir/cmd-custom/defvar.fish
+echo "set -x DOTFILES_DIR $HOME/dotfiles"                  >> $script_dir/cmd-custom/defvar.fish
+echo "set -x DOTFILES_PKGS_DIR $script_dir/config/pkgs"    >> $script_dir/cmd-custom/defvar.fish
+echo "set -x DROPBOX_DIR $dropbox_dir"                     >> $script_dir/cmd-custom/defvar.fish
 
 fisher install hasanozgan/theme-lambda
 fisher install gitignore
