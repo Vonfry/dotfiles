320bc303154c8445c965bb5b3b62bed9a7a9c183
Vonfry <vonfry314@gmail.com>
2018-05-04 19:42:25 +0800

    feat(setup): lsp
    ln

--- setup/31lsp.sh
+++ setup/31lsp.sh
@@ -1,6 +1,7 @@
 git clone --single-branch --depth=1 https://github.com/cquery-project/cquery ~/.local/src/cquery
 cd ~/.local/src/cquery
 git submodule update --init
+ln -s ~/.local/src/cquery/build/release/bin/cquery ~/.local/bin
 
 git clone --single-branch --depth=1 https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
 cd ~/.local/src/haskell/ide-engine
