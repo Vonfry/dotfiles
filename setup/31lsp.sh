07410d2e3c8516960d876dc86d95d2a4e5f4e48b
Vonfry <vonfry314@gmail.com>
2018-05-04 20:00:13 +0800

    fix(setup): lsp

--- setup/31lsp.sh
+++ setup/31lsp.sh
@@ -1,11 +1,9 @@
-git clone --single-branch --depth=1 https://github.com/cquery-project/cquery ~/.local/src/cquery
+git clone --single-branch --depth=1 --recursive https://github.com/cquery-project/cquery ~/.local/src/cquery
 cd ~/.local/src/cquery
-git submodule update --init
 ln -s ~/.local/src/cquery/build/release/bin/cquery ~/.local/bin
 
-git clone --single-branch --depth=1 https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
+git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
 cd ~/.local/src/haskell/ide-engine
-git submodule update --init
 stack install
 
 cd $script_dir
