ec47d95175cc6a00bc158590d258af3a812fee77
Vonfry <vonfry314@gmail.com>
2018-06-06 17:10:04 +0800

    fix(clean): vim
    remove sth an used plugin

--- setup/31vim.sh
+++ setup/31vim.sh
@@ -2,6 +2,6 @@ echo_info "** setup vim"
 ln -f -s $script_dir/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c "execute \"PluginInstall!\" | qa"
-~/.vim/bundle/YouCompleteMe/install.py --tern-completer --clang-completer --system-boost --system-libclang
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-boost --system-libclang
 mkdir -p ~/.cache/vim
 mkdir -p ~/.cache/vim/undodir
