584a9b95d0a4255d71cb74907a1eb66ebbd99eb8
Vonfry <vonfry314@gmail.com>
2018-06-06 20:31:15 +0800

    fix(init): xinitrc
    use cp

--- setup/50x.sh
+++ setup/50x.sh
@@ -1,2 +1,6 @@
-ln -f -s $script_dir/xinitrc ~/.xinitrc
 ln -f -s $script_dir/Xresources ~/.Xresources
+case "$(unname)" in
+    "Darwin")
+        ln -s -f ~/opt/X11/lib/X11/xinit/xinitrc ~/.xinitrc
+        ;;
+esac
