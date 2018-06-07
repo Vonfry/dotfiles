81945e3de120419e0d7eceb753a3cd09253ce0ee
Vonfry <vonfry314@gmail.com>
2018-06-07 19:24:59 +0800

    feat(emacs): lsp
    javascript

--- setup/31lsp.sh
+++ setup/31lsp.sh
@@ -1,10 +1,19 @@
-git clone --single-branch --depth=1 --recursive https://github.com/cquery-project/cquery ~/.local/src/cquery
-cd ~/.local/src/cquery
-ln -s ~/.local/src/cquery/build/release/bin/cquery ~/.local/bin
+if command -v llvm-config > /dev/null 2>&1;  then
+    git clone --single-branch --depth=1 --recursive https://github.com/cquery-project/cquery ~/.local/src/cquery
+    cd ~/.local/src/cquery
+    cmake -DSYSTEM_CLANG=ON  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)  -DCMAKE_PREFIX_PATH=$(llvm-config --prefix) -DCMAKE_INSTALL_PREFIX=~/.local/bin
+    make && make install
+fi
 
-git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
-cd ~/.local/src/haskell/ide-engine
-stack install
-stack exec hoogle generate
+if command -v stack > /dev/null 2>&1;  then
+    git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
+    cd ~/.local/src/haskell/ide-engine
+    stack install
+    stack exec hoogle generate
+fi
+
+if command -v npm > /dev/null 2>&1;  then
+    npm install -g javascript-typescript-langserver
+fi
 
 cd $script_dir
