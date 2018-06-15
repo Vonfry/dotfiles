e2d360289b1c1456ec262953eb61c31bac63c869
Vonfry <vonfry314@gmail.com>
2018-06-15 17:45:18 +0800

    fix(emacs): fold
    and rename some

--- emacs.d/modules/visual/fold/packages.el
+++ emacs.d/modules/visual/fold/packages.el
@@ -2,4 +2,27 @@
 ;;
 
 (package! origami
-  :hook ((text-mode prog-mode) . origami-mode))
+  :hook ((text-mode prog-mode) . origami-mode)
+  :general
+  (nmap :prefix +nmap-origaml-prefix
+        :keymaps '(text-mode prog-mode)
+        "o" 'origami-open-node
+        "O" 'origami-open-node-recursively
+        "s" 'origami-show-node
+        "c" 'origami-close-node
+        "C" 'origami-close-node-recursively
+        "a" 'origami-toggle-node
+        "P" 'origami-forward-toggle-node
+        "A" 'origami-recursively-toggle-node
+        "r" 'origami-open-all-nodes
+        "m" 'origami-close-all-nodes
+        "t" 'origami-toggle-all-nodes
+        "S" 'origami-show-only-node
+        "p" 'origami-previous-fold
+        "n" 'origami-next-fold
+        "f" 'origami-forward-fold
+        "F" 'origami-forward-fold-same-level
+        "b" 'origami-backward-fold-same-level
+        "u" 'origami-undo
+        "d" 'origami-redo
+        "e" 'origami-reset))
