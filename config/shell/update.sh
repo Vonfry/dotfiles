eb6aa4056b9460d59bc9ca119f8aaf72df50bc21
Vonfry <vonfry314@gmail.com>
2018-08-19 10:26:42 +0800

    fix(gentoo): remove
    layman

--- config/shell/update.sh
+++ config/shell/update.sh
@@ -39,11 +39,9 @@ function vonfry-update()
                 "gentoo")
                     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}portage${ECHO_RST}"
                     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}sync portage and custom repos${ECHO_RST}\n"
-                    sudo eix-sync -q # This action contains portage/layman sync.
+                    sudo eix-sync -q
                     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}portage self${ECHO_RST}\n"
                     sudo emerge --update --oneshot --quiet portage
-                    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}layman${ECHO_RST}\n"
-                    sudo haskell-updater --quiet
                     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}portage worlds${ECHO_RST}"
                     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}If use flag has been changed, run emerge with --newuse or --changed-use. This updating dosen't contain them.${ECHO_RST}\n"
                     sudo emerge --quiet --update --deep --with-bdeps=y @world
