29e89c029c94ad24b40b69d8b998b2f74eeda192
Vonfry <vonfry314@gmail.com>
2018-09-15 09:50:29 +0800

    fix(emacs): ivy
    rich

--- emacs.d/modules/interface/ivy/packages.el
+++ emacs.d/modules/interface/ivy/packages.el
@@ -18,17 +18,8 @@
         +nmap-swiper 'swiper
         +nmap-marks 'counsel-mark-ring
         +nmap-buffer-switch 'ivy-switch-buffer))
+
 (package! ivy-rich
   :after counsel
   :config
-  (advice-add 'ivy-rich-switch-buffer-pad :override
-              (lambda (str len &optional left)
-                "Improved version of `ivy-rich-switch-buffer-pad' that truncates long inputs."
-                (let ((real-len (length str)))
-                  (cond
-                    ((< real-len len) (if left
-                                        (concat (make-string (- len real-len) ? ) str)
-                                        (concat str (make-string (- len real-len) ? ))))
-                    ((= len real-len) str)
-                    (t (concat (substring str 0 (- len 1)) "…"))))))
-  (ivy-set-display-transformer 'ivy-switch-buffer 'ivy-rich-switch-buffer-transformer))
+  (ivy-rich-mode 1))
