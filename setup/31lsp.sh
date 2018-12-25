f58e84da58afc0c556db36fb80c74e049e8ff8df
Vonfry <vonfry314@gmail.com>
2018-12-25 15:36:07 +0800

    fix(emacs): lsp
    change ruby lsp, use clangd instead of cquery

--- setup/31lsp.sh
+++ setup/31lsp.sh
@@ -1,12 +1,3 @@
-if command -v llvm-config > /dev/null 2>&1;  then
-    git clone --single-branch --depth=1 --recursive https://github.com/cquery-project/cquery ~/.local/src/cquery
-    cd ~/.local/src/cquery
-    mkdir build && cd ./build
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local/bin -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
-    cmake --build .
-    cmake --build . --target install
-fi
-
 if command -v stack > /dev/null 2>&1;  then
     git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
     cd ~/.local/src/haskell/ide-engine
