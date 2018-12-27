12652349d18699d5b30c2f38cd74a8e680887e1d
Vonfry <vonfry314@gmail.com>
2018-12-27 10:23:22 +0800

    feat(emacs): ruby
    lsp

--- emacs.d/modules/completion/lsp/packages.el
+++ emacs.d/modules/completion/lsp/packages.el
@@ -17,7 +17,14 @@
 
 (package! lsp-clients
   :after lsp-mode
-  :ensure nil)
+  :ensure nil
+  :config
+  (lsp-register-client
+    (make-lsp-client :new-connection (lsp-stdio-connection '("solargraph" "stdio"))
+                     :major-modes '(enh-ruby-mode)
+                     :priority -1
+                     :multi-root t
+                     :server-id 'ruby-ls)))
 
 (package! lsp-ui
   :after lsp-mode evil
