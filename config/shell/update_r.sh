7245c343de6f7f9d1ba8f707fb5544be35eaad4e
Vonfry <vonfry314@gmail.com>
2019-01-01 09:45:39 +0800

    fix(gentoo): update
    remove deep

--- config/shell/update_r.sh
+++ config/shell/update_r.sh
@@ -36,7 +36,7 @@ case "$(uname)" in
                 sudo emerge --update --oneshot --quiet portage
                 echo -e "\n${ECHO_SYM}** ${ECHO_MSG}portage worlds${ECHO_RST}"
                 echo -e "\n${ECHO_SYM}** ${ECHO_MSG}If use flag has been changed, run emerge with --newuse or --changed-use. This updating dosen't contain them.${ECHO_RST}\n"
-                sudo emerge --quiet --update --deep --with-bdeps=y @world
+                sudo emerge --quiet --update --with-bdeps=y @world
                 echo -e "\n${ECHO_SYM}** ${ECHO_MSG}Run emerge --depclean --quiet if you want to clean old packages.${ECHO_RST}\n"
                 ;;
         esac
