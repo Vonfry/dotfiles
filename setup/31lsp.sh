2fbd024b31abe51e96913b2d532f8c16ec81fcdd
Vonfry <vonfry314@gmail.com>
2019-01-16 16:04:02 +0800

    fix(hie): install
    remove

--- setup/31lsp.sh
+++ setup/31lsp.sh
@@ -1,12 +1,9 @@
 if command -v stack > /dev/null 2>&1;  then
     git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
     cd ~/.local/src/haskell/ide-engine
-    stack install
-    stack exec hoogle generate
-fi
-
-if command -v npm > /dev/null 2>&1;  then
-    npm install -g javascript-typescript-langserver
+    echo_info "--- Install HIE by self because of unstable installing method."
+    # stack install
+    # stack exec hoogle generate
 fi
 
 cd $script_dir
