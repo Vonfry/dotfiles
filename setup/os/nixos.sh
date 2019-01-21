7ca719a7d654bd47fe199158d2b2c01a2a817b02
Vonfry <vonfry314@gmail.com>
2019-01-21 20:41:36 +0800

    fix(nixos): setup
    shell replace

--- setup/os/nixos.sh
+++ setup/os/nixos.sh
@@ -1,8 +1,14 @@
 sudo nixos-generate-config
 sudo cp -r $script_dir/etc/nixos/vonfry /etc/nixos/
-sudo cp /etc/nixos/vonfry/base.local.nix.example /etc/nixos/vonfry/base.local.nix
+find . -name "*.local.nix.example" |
+    xargs -n1 -I "{}" sed /\.example$/d |
+    xargs -n1 -I "{}" sudo cp {}.example {}
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
 sudo cp $script_dir/etc/nixos/configuration.nix /etc/nixos/configuration.nix
-sed -i "/^ *# *system\.stateVersion$/a $(grep -e "^ *system\.stateVersion" /etc/nixos/configuration.nix.bak)" /etc/nixos/vonfry/base.local.nix
-sed -i "/^ *# boot\.loader$/a $(grep -e "^ *boot\.loader" /etc/nixos/configuration.nix.bak)" /etc/nixos/vonfry/base.local.nix
+sudo grep -e "^ *system\.stateVersion" /etc/nixos/configuration.nix.bak |
+    xargs -n1 -I "{}" sed "s/ /\\\\ /g" |
+    xargs -n1 -I "{}" sed -i "/^ *# *system\.stateVersion$/a {}" /etc/nixos/vonfry/base.local.nix
+sudo grep -e "^ *boot\.loader" /etc/nixos/configuration.nix.bak |
+    xargs -n1 -I "{}" sed "s/ /\\\\ /g" |
+    xargs -n1 -I "{}" sed -i "/^ *# boot\.loader$/a " /etc/nixos/vonfry/base.local.nix
 sudo nixos-rebuild switch
