3b5d7f69cdeaa9b0ae382b2287926b5dc99c623f
Vonfry <vonfry314@gmail.com>
2019-01-23 16:07:23 +0800

    fix(git): remove diff so fancy by default

--- setup/shell/git.sh
+++ setup/shell/git.sh
@@ -1,9 +1,4 @@
 echo_info "** setup git"
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global color.diff-highlight.oldNormal "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
 git config --global core.fileMode true
 git config --global color.ui true
 
