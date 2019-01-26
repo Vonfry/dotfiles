cf3e0d928fd11120aff3f39f53246d05d1ef32e2
Vonfry <vonfry314@gmail.com>
2019-01-26 16:00:40 +0800

    remove nixos

--- etc/nixos/vonfry/user/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  imports = let users = [ ./vonfry.nix ]; in
-    with import (<nixos-config> + "/../vonfry/libs");
-       mergeList users (matchFiles ./. "[A-z0-9]*\\.local\\.nix" []);
-}
--- etc/nixos/vonfry/user/vonfry.local.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  users.users.vonfry.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpHbr9kE9gybnF7a9I8eb9Oa/1GEMj759MlSueRpXL1 vonfry314@gmail.com" ];
-  security.sudo.extraConfig = "Defaults:vonfry timestamp_timeout=-1";
-}
