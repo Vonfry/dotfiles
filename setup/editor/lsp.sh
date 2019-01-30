138b205991dee637dd0751af9f4f4afce61283f6
Vonfry <mail@vonfry.name>
2019-01-30 15:46:29 +0800

    lsp: install haskell ide engine with nix instead of stack

--- setup/editor/lsp.sh
+++ setup/editor/lsp.sh
@@ -1,9 +1,6 @@
-if command -v stack > /dev/null 2>&1;  then
-    git clone --single-branch --depth=1 --recursive https://github.com/haskell/haskell-ide-engine ~/.local/src/haskell-ide-engine
-    cd ~/.local/src/haskell/ide-engine
-    echo_info "--- Install HIE by self because of unstable installing method."
-    # stack install
-    # stack exec hoogle generate
+if command -v nix-env > /dev/null 2>&1;  then
+    echo_info "--- Install HIE by nix."
+    nix-env -iA hie84 -f https://github.com/domenkozar/hie-nix/tarball/master
 fi
 
 cd $script_dir
