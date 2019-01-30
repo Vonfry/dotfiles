dc9130a269a7243391d781542461023f866fd64a
Vonfry <mail@vonfry.name>
2019-01-30 13:44:10 +0800

    setup: ruby

--- setup/pkg/ruby.sh
+++ setup/pkg/ruby.sh
@@ -2,7 +2,7 @@ echo_info "** setup ruby"
 
 if command -v gem > /dev/null 2>&1; then
     echo_info "*** setup gem"
-    gem install --silent --no-lock --user-install --bindir ~/.local/bin -g $script_dir/config/pkgs/gem.txt
+    gem install --quiet --no-lock --user-install --bindir ~/.local/bin -g $script_dir/config/pkgs/gem.txt
     if command -v bundle > /dev/null 2>&1; then
         bundle config console pry
     else
