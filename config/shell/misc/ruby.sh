2064ef2e03c6df1c6f6b190b1e793c1b2ed1742d
Vonfry <mail@vonfry.name>
2019-02-02 13:35:38 +0800

    ruby gem: remove some tools using in project and change install dir

---
+++ config/shell/misc/ruby.sh
@@ -0,0 +1,1 @@
+export PATH=~/.gem/bin:$PATH
--- setup/pkg/ruby.sh
+++ setup/pkg/ruby.sh
@@ -1,8 +1,10 @@
 echo_info "** setup ruby"
 
+ln $script_dir/gemrc ~/.gemrc
+mkdir -p ~/.gem/bin
 if command -v gem > /dev/null 2>&1; then
     echo_info "*** setup gem"
-    gem install --quiet --no-lock --user-install --bindir ~/.local/bin -g $script_dir/config/pkgs/gem.txt
+    gem install --quiet --no-lock --user-install -g $script_dir/config/pkgs/gem.txt
     if command -v bundle > /dev/null 2>&1; then
         bundle config console pry
     else
