a8bf4b97f453ceb4f71625a852ed13b8d2fb32f1
Vonfry <mail@vonfry.name>
2019-02-09 11:12:17 +0800

    update: git path and current path

--- config/shell/update/default.sh
+++ config/shell/update/default.sh
@@ -6,6 +6,4 @@ $SHELL $DOTFILES_DIR/config/update/editor.sh
 echo -e "\n${ECHO_SYM}* ${ECHO_MSG}update end ${ECHO_RST}\n"
 echo -e "\n${ECHO_SYM}-- ${ECHO_MSG}Please do a check. Some action may failed, specially what need to being built at local, such as gentoo.${ECHO_RST}"
 
-cd $current_dir
-
 echo $(($(date +%s) / 60 / 60 / 24)) >! $SHELL_CUSTOM_DIR/local/.update_epoch
