333077bbf0a77f015a6902bce76907a9cbc481cd
Vonfry <mail@vonfry.name>
2019-02-09 16:19:03 +0800

    xmonad & xinput

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,24 @@
 import XMonad
-import XMonad.Hooks.ManageDocks (docks)
-import XMonad.Util.Run (spawnPipe)
+import qualified XMonad.Prompt as XP
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Layout.ShowWName (showWName)
+import qualified XMonad.Layout.ShowWName as XLS
+import qualified Data.Map as M
 import System.IO
+
+myXPC = XP.def
+    { XP.font = "xft:Hack-11"
+    }
+
+myKeys conf@(XConfig {modMask = modm}) = M.fromList
+    [ ((modm .|. shiftMask, xK_x), shellPrompt myXPC)
+    ]
 
 myDef = def
     { modMask = mod4Mask
     , terminal = "xterm"
+    , keys =  myKeys <+> keys def
+    , layoutHook = showWName $ layoutHook XLS.def
     }
 
 main = xmonad myDef
