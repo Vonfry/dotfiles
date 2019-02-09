1230ba06b413fc66ba1e81d9d1f9d4ef45078502
Vonfry <mail@vonfry.name>
2019-02-10 00:14:05 +0800

    xmonad

--- setup/x/default.sh
+++ setup/x/default.sh
@@ -1,10 +1,15 @@
 echo_info "* X"
 ln -f -s $script_dir/xsession ~/.xsession
 ln -f -s $script_dir/Xresources ~/.Xresources
+ln -f -s $script_dir/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 case "$(unname)" in
     "Darwin")
         ln -s -f /opt/X11/lib/X11/xinit/xinitrc ~/.xinitrc
         ;;
 esac
+
+if command -v xmonad &> /dev/null; then
+    xmonad --recompile
+fi
 
 echo_note "--- Default font: Hack(en), Source Han Sans(cn). You need to installing them by yourself."
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,27 +1,92 @@
 import XMonad
-import qualified XMonad.Prompt as XP
-import XMonad.Prompt.Shell (shellPrompt)
-import XMonad.Prompt.Window (windowPrompt, WindowPrompt(Bring), allWindows, wsWindows)
-import XMonad.Layout.ShowWName (showWName)
-import qualified XMonad.Layout.ShowWName as XLS
-import qualified Data.Map as M
+import XMonad.Actions.Submap
+import XMonad.Actions.Search
+import XMonad.Actions.WindowMenu
+import XMonad.Actions.Launcher
+import XMonad.Actions.GridSelect
+import XMonad.Actions.Navigation2D
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+import XMonad.Prompt.Layout
+import XMonad.Prompt.FuzzyMatch
+import XMonad.Prompt.Window
+import XMonad.Layout.Grid
+import XMonad.Layout.ShowWName
+import XMonad.Layout.WindowArranger
+import qualified  Data.Map as M
 import System.IO
 
-myXPC = XP.def
-    { XP.font = "xft:Hack-11"
+myFont = "xft:Hack-11"
+myModMask = mod4Mask
+
+myXPC = def
+    { font = myFont
+    , searchPredicate = fuzzyMatch
+    }
+
+myGSC = def
+    { gs_font = myFont
     }
 
 myKeys conf@(XConfig {modMask = modm}) = M.fromList
-    [ ((modm .|. shiftMask, xK_x), shellPrompt myXPC)
-    , ((modm .|. shiftMask, xK_w), windowPrompt myXPC Bring allWindows)
-    , ((modm, xK_w), windowPrompt myXPC Bring wsWindows)
+    [ ((modm, xK_x         ), shellPrompt  myXPC)
+    , ((modm, xK_apostrophe), xmonadPrompt myXPC)
+    , ((modm, xK_slash     ), promptSearch myXPC multi)
+
+    , ((modm, xK_space), switchLayer)
+    , ((modm, xK_l), windowGo R False)
+    , ((modm, xK_h), windowGo L False)
+    , ((modm, xK_k), windowGo U False)
+    , ((modm, xK_j), windowGo D False)
+    , ((modm .|. controlMask, xK_l), windowSwap R False)
+    , ((modm .|. controlMask, xK_h), windowSwap L False)
+    , ((modm .|. controlMask, xK_k), windowSwap U False)
+    , ((modm .|. controlMask, xK_j), windowSwap D False)
+    , ((modm .|. shiftMask, xK_l), screenGo R False)
+    , ((modm .|. shiftMask, xK_h), screenGo L False)
+    , ((modm .|. shiftMask, xK_k), screenGo U False)
+    , ((modm .|. shiftMask, xK_j), screenGo D False)
+    , ((modm .|. shiftMask .|. controlMask, xK_l), screenSwap R False)
+    , ((modm .|. shiftMask .|. controlMask, xK_h), screenSwap L False)
+    , ((modm .|. shiftMask .|. controlMask, xK_k), screenSwap U False)
+    , ((modm .|. shiftMask .|. controlMask, xK_j), screenSwap D False)
+    , ((modm .|. mod1Mask, xK_l), windowToScreen R False)
+    , ((modm .|. mod1Mask, xK_h), windowToScreen L False)
+    , ((modm .|. mod1Mask, xK_k), windowToScreen U False)
+    , ((modm .|. mod1Mask, xK_j), windowToScreen D False)
+
+    , ((modm .|. controlMask              , xK_s    ), sendMessage  Arrange         )
+    , ((modm .|. controlMask .|. shiftMask, xK_s    ), sendMessage  DeArrange       )
+    , ((modm .|. controlMask              , xK_Left ), sendMessage (MoveLeft      1))
+    , ((modm .|. controlMask              , xK_Right), sendMessage (MoveRight     1))
+    , ((modm .|. controlMask              , xK_Down ), sendMessage (MoveDown      1))
+    , ((modm .|. controlMask              , xK_Up   ), sendMessage (MoveUp        1))
+    , ((modm                 .|. shiftMask, xK_Left ), sendMessage (IncreaseLeft  1))
+    , ((modm                 .|. shiftMask, xK_Right), sendMessage (IncreaseRight 1))
+    , ((modm                 .|. shiftMask, xK_Down ), sendMessage (IncreaseDown  1))
+    , ((modm                 .|. shiftMask, xK_Up   ), sendMessage (IncreaseUp    1))
+    , ((modm .|. controlMask .|. shiftMask, xK_Left ), sendMessage (DecreaseLeft  1))
+    , ((modm .|. controlMask .|. shiftMask, xK_Right), sendMessage (DecreaseRight 1))
+    , ((modm .|. controlMask .|. shiftMask, xK_Down ), sendMessage (DecreaseDown  1))
+    , ((modm .|. controlMask .|. shiftMask, xK_Up   ), sendMessage (DecreaseUp    1))
+
+    , ((modm .|. controlMask, xK_m), layoutPrompt myXPC)
+    , ((modm, xK_g), goToSelected myGSC)
+    , ((modm, xK_b), bringSelected myGSC)
+
+    , ((modm, xK_o), windowMenu)
     ]
 
+myLayout = windowArrangeAll $ Grid ||| Full ||| layoutHook def
+
+myNav2D = withNavigation2DConfig def
+
 myDef = def
-    { modMask = mod4Mask
+    { modMask = myModMask
     , terminal = "xterm"
     , keys =  myKeys <+> keys def
-    , layoutHook = showWName $ layoutHook XLS.def
+    , layoutHook = myLayout
     }
 
-main = xmonad myDef
+main = xmonad $ myNav2D $ myDef
