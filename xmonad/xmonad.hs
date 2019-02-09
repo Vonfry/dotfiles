7715294551595b623b557c5a6932a3bce0d9a8b2
Vonfry <mail@vonfry.name>
2019-02-09 21:54:17 +0800

    nixos: x & input

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import qualified XMonad.Prompt as XP
 import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Prompt.Window (windowPrompt, WindowPrompt(Bring), allWindows, wsWindows)
 import XMonad.Layout.ShowWName (showWName)
 import qualified XMonad.Layout.ShowWName as XLS
 import qualified Data.Map as M
@@ -12,6 +13,8 @@ myXPC = XP.def
 
 myKeys conf@(XConfig {modMask = modm}) = M.fromList
     [ ((modm .|. shiftMask, xK_x), shellPrompt myXPC)
+    , ((modm .|. shiftMask, xK_w), windowPrompt myXPC Bring allWindows)
+    , ((modm, xK_w), windowPrompt myXPC Bring wsWindows)
     ]
 
 myDef = def
