9821a65c6254aad28693317b0f078760e618ac1e
Vonfry <mail@vonfry.name>
2019-02-10 12:12:50 +0800

    update: haskell remove stack

--- config/shell/update/pkgs/haskell.sh
+++ config/shell/update/pkgs/haskell.sh
@@ -3,10 +3,6 @@ if command -v cabal > /dev/null 2>&1; then
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}cabal${ECHO_RST}\n"
     cabal update --verbose=0
 fi
-if command -v stack > /dev/null 2>&1; then
-    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}stack${ECHO_RST}\n"
-    stack update --silent
-fi
 if command -v hoogle > /dev/null 2>&1; then
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}hoogle${ECHO_RST}\n"
     hoogle generate --quiet
