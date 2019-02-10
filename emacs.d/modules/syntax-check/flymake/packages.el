78728b2f4ee38ef9265735035a11781be805286c
Vonfry <mail@vonfry.name>
2019-02-10 22:35:19 +0800

    emacs: use flymake with lsp instead of flycheck

---
+++ emacs.d/modules/syntax-check/flymake/packages.el
@@ -0,0 +1,9 @@
+;;; flycheck packages -*- lexical-binding: t -*-
+;;
+
+(package! flymake
+  :general
+  (nmap :keymaps 'prog-mode-map
+        :prefix +nmap-leader
+        +nmap-check 'flymake-show-diagnostic))
+
