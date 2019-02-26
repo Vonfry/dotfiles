83eb579b738df5a47e8456e9e42e011ed02d558f
Vonfry <mail@vonfry.name>
2019-02-26 14:29:25 +0800

    remove xresources

--- setup/x/default.sh
+++ setup/x/default.sh
@@ -1,15 +1,13 @@
-echo_info "* X"
-ln -f -s $script_dir/xsession ~/.xsession
-ln -f -s $script_dir/Xresources ~/.Xresources
-ln -f -s $script_dir/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-case "$(unname)" in
-    "Darwin")
-        ln -s -f /opt/X11/lib/X11/xinit/xinitrc ~/.xinitrc
-        ;;
-esac
+if [ $(uname) = "Linux" ]; then
+    echo_info "* X"
+    ln -f -s $script_dir/xsession ~/.xsession
+    ln -f -s $script_dir/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+    mkdir -p ~/.config/alacritty
+    ln -f -s $script_dir/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-if command -v xmonad &> /dev/null; then
-    xmonad --recompile
+    if command -v xmonad &> /dev/null; then
+        xmonad --recompile
+    fi
+
+    echo_note "--- Default font: Hack(en), Source Han Sans(cn). You need to installing them by yourself."
 fi
-
-echo_note "--- Default font: Hack(en), Source Han Sans(cn). You need to installing them by yourself."
