5f0930f887ac25eb2d742e2f04c253a6d91a3f1b
Vonfry <mail@vonfry.name>
2019-03-01 11:03:32 +0800

    remove pip2

--- config/shell/update/pkgs/python.sh
+++ config/shell/update/pkgs/python.sh
@@ -3,9 +3,3 @@ if command -v pip3 > /dev/null 2>&1; then
     echo -e "\n${ECHO_SYM}** ${ECHO_MSG}pip3${ECHO_RST}\n"
     pip3 install --quiet --user --upgrade -r $DOTFILES_DIR/config/pkgs/pip3.txt
 fi
-
-if command -v pip2 > /dev/null 2>&1; then
-    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}pip2${ECHO_RST}\n"
-    pip2 install --quiet --user --upgrade -r $DOTFILES_DIR/config/pkgs/pip2.txt
-fi
-
