91db9de31ff8290913d9319c4d15160351d52756
Vonfry <mail@vonfry.name>
2019-04-08 15:54:43 +0800

    nixos shell config

--- config/shell/os.sh
+++ config/shell/os.sh
@@ -4,7 +4,5 @@ case "$(uname)" in
     "Darwin") source $SHELL_CUSTOM_DIR/os/macos.sh ;;
     "Linux")
         source /etc/os-release
-        case "$ID" in
-            "gentoo") source $SHELL_CUSTOM_DIR/os/gentoo.sh ;;
-        esac ;;
+        source $SHELL_CUSTOM_DIR/os/$ID.sh ;;
 esac
