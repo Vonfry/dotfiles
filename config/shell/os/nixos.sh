08ea71c7ef8a41fe5de68ec1b417c573c295c120
Vonfry <mail@vonfry.name>
2019-04-08 15:55:54 +0800

    fix shell

--- config/shell/os/nixos.sh
+++ config/shell/os/nixos.sh
@@ -2,12 +2,12 @@ if [ -n "${BASH}" ]; then
     shell="bash"
 elif [ -n "${ZSH_NAME}" ]; then
     shell="zsh"
-    --  elif [ -n "${__fish_datadir}" ]; then
+elif [ -n "${__fish_datadir}" ]; then
     shell="fish"
-    --  elif [ -n "${version}" ]; then
+elif [ -n "${version}" ]; then
     shell="tcsh"
 else
-    --      shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
+    shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
 fi
 if  command -v autojump > /dev/null 2>&1; then
     source $(nixos nix-store -q $(nix-store -qd $(which autojump)))/share/autojump/autojump.$shell
