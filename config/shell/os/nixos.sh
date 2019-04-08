198cbba24ad80497a3d3336bc3ef05dce9e4f5a3
Vonfry <mail@vonfry.name>
2019-04-08 15:56:53 +0800

    fix

--- config/shell/os/nixos.sh
+++ config/shell/os/nixos.sh
@@ -10,6 +10,6 @@ else
     shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
 fi
 if  command -v autojump > /dev/null 2>&1; then
-    source $(nixos nix-store -q $(nix-store -qd $(which autojump)))/share/autojump/autojump.$shell
+    source $(nix-store -q $(nix-store -qd $(which autojump)))/share/autojump/autojump.$shell
 fi
 unset shell
