ea432699321a679fd0bb159652fb73bcfd2316ff
Vonfry <mail@vonfry.name>
2019-05-07 10:50:44 +0800

    nixos: fix custom

--- etc/nixos/user/vonfry/packages/default.nix
+++ etc/nixos/user/vonfry/packages/default.nix
@@ -4,7 +4,7 @@ let lib = import <nixos-vonfry-lib>; in
 {
   imports = lib.matchFiles ./. "default\\.local\\.nix" [];
   users.users.vonfry.packages =
-    let args = { pkgs = pkgs; customPkgs = pkgs.callPackage ./custom; };
+    let args = { pkgs = pkgs; customPkgs = pkgs.callPackage ./custom {}; };
         importList = [ ./misc.nix ./shell.nix ./dev ./media.nix ];
     in builtins.foldl' (x: y: x ++ import y args) [] importList;
 }
