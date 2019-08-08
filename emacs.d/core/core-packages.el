624784f697d0b552aa447e69c0e42dd745ffbc9b
Vonfry <mail@vonfry.name>
2019-08-08 14:05:42 +0800

    initialize

--- emacs.d/core/core-packages.el
+++ emacs.d/core/core-packages.el
@@ -56,8 +56,8 @@ is undefined(It always is loaded by alpha order)."
   '(use-package-always-ensure t))
 
 (eval-when-compile (require 'package))
-(package-initialize)
 (setq package-archives vonfry-elpa-mirror)
+(package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
