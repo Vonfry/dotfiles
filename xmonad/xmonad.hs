2a78b974cd87c995a288f02dc57016556cc40120
Vonfry <mail@vonfry.name>
2019-09-11 19:15:24 +0800

    xmonad scrot command

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -44,9 +44,9 @@ myKeys conf@(XConfig {modMask = modm}) = M.fromList
     , ((modm, xK_apostrophe), xmonadPrompt myXPC)
     , ((modm, xK_slash     ), promptSearch myXPC multi)
 
-    , ((modm                , xK_Print), spawn "scrot --focused '~/.local/screenshot/window_%s_$wx$h.png'")
-    , ((modm .|. shiftMask  , xK_Print), spawn "scrot '~/.local/screenshot/screen_%s_$wx$h.png'")
-    , ((modm .|. controlMask, xK_Print), spawn "scrot --select '~/.local/screenshot/select_%s_$wx$h.png'")
+    , ((modm                , xK_Print), spawn "scrot --focused 'window_%s_$wx$h.png' -e 'mv $f ~/.local/screenshot/'")
+    , ((modm .|. shiftMask  , xK_Print), spawn "scrot 'screen_%s_$wx$h.png' -e mv ~/.local/screenshot/")
+    , ((modm .|. controlMask, xK_Print), spawn "scrot --select 'select_%s_$wx$h.png' -e 'mv $f ~/.local/screenshot/'")
 
     , ((modm .|. shiftMask, xK_t), withFocused float)
 
