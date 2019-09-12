6d0cfb6fe8d7de002df156cc5c097ebd1b35a46c
Vonfry <mail@vonfry.name>
2019-09-12 16:49:11 +0800

    xmonad: change screen dir

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -44,9 +44,9 @@ myKeys conf@(XConfig {modMask = modm}) = M.fromList
     , ((modm, xK_apostrophe), xmonadPrompt myXPC)
     , ((modm, xK_slash     ), promptSearch myXPC multi)
 
-    , ((modm                , xK_Print), spawn "scrot --focused 'window_%s_$wx$h.png' -e 'mv $f ~/.local/screenshot/'")
-    , ((modm .|. shiftMask  , xK_Print), spawn "scrot 'screen_%s_$wx$h.png' -e 'mv ~/.local/screenshot/'")
-    , ((modm .|. controlMask, xK_Print), spawn "scrot --select 'select_%s_$wx$h.png' -e 'mv $f ~/.local/screenshot/'")
+    , ((modm                , xK_Print), spawn "scrot --focused 'window_%s_$wx$h.png' -e 'mv $f ~/screenshot/'")
+    , ((modm .|. shiftMask  , xK_Print), spawn "scrot 'screen_%s_$wx$h.png' -e 'mv ~/screenshot/'")
+    , ((modm .|. controlMask, xK_Print), spawn "scrot --select 'select_%s_$wx$h.png' -e 'mv $f ~/screenshot/'")
 
     , ((modm .|. shiftMask, xK_t), withFocused float)
 
