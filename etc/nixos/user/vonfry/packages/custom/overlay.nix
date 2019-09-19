ae7b3867ae4fc911e5f39d68f6fab0a202c68071
Vonfry <mail@vonfry.name>
2019-09-20 07:45:48 +0800

    overlay

--- etc/nixos/user/vonfry/packages/custom/overlay.nix
+++ etc/nixos/user/vonfry/packages/custom/overlay.nix
@@ -2,5 +2,8 @@ self: super:
 
 with super;
 {
+  python = callPackage ./dev-lang/python { python = python3; };
+  rubyPkgs = callPackage ./dev-ruby/self-pkgs { };
   hie = callPackage ./dev-haskell/hie { };
+  xmonad = callPackage ./x11-wm/xmonad { };
 }
