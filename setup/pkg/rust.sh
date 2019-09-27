d1e49363b8050a75d184e1b327a7fcae4d2b3162
Vonfry <mail@vonfry.name>
2019-09-27 13:14:59 +0800

    rust setup

--- setup/pkg/rust.sh
+++ setup/pkg/rust.sh
@@ -1,14 +1,12 @@
-if ! [ -f /etc/NIXOS ]; then
-    if command -v rustup >/dev/null 2>&1; then
-        echo_info "*** setup pip3"
-        rustup install stable
-        rustup default stable
-        rustup update
-        cat $script_dir/config/pkgs/rust.txt | xargs -n1 rustup component add
-    else
-        echo_warn "!!! ERROR: NO pip3"
-        exit 1
-    fi
+if command -v rustup >/dev/null 2>&1; then
+    echo_info "*** setup rustup"
+    rustup install stable
+    rustup default stable
+    rustup update
+    cat $script_dir/config/pkgs/rust.txt | xargs -n1 rustup component add
+elif command -v cargo >/dev/null 2>&1; then
+    cat $script_dir/config/pkgs/rustup.txt | xargs -n1 cargo install --quiet
 else
-    echo_info "--- NixOS. Use configuration to manage."
+    echo_warn "!!! ERROR: NO rust"
+    exit 1
 fi
