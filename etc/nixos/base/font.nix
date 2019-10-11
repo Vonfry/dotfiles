d6f0aefeeb3e5d99cc3e95f4db9d9d571729d7eb
Vonfry <mail@vonfry.name>
2019-10-11 14:52:47 +0800

    nixos: fix restruct

---
+++ etc/nixos/base/font.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ...}:
+
+{
+  fonts = {
+    fonts = with pkgs; [ hack-font source-han-sans-simplified-chinese ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Hack" ];
+	sansSerif = [ "Source Han Sans CN" ];
+	serif = [];
+      };
+    };
+  };
+}
