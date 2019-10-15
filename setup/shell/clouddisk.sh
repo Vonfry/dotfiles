bb19f5926a1178165b8770b90b8adeae12610c25
Vonfry <mail@vonfry.name>
2019-10-15 12:31:59 +0800

    setup: nextcloud

--- setup/shell/clouddisk.sh
+++ setup/shell/clouddisk.sh
@@ -1,6 +1,4 @@
 echo_info "** cloud disk"
-if ! [ command -v nextcloud > /dev/null 2>&1 ]; then
-    if [[ $(uname) = "Linux" && ! -f /etc/NIXOS ]]; then
-        echo_note "-- this tool uses appimage, please download and install it by yourself into ~/.locol/bin"
-    fi
+if [[ $(uname) = "Linux" && ! -f /etc/NIXOS ]]; then
+    echo_note "-- this tool uses appimage, please download and install it by yourself into ~/.locol/bin"
 fi
