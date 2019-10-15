43582a47318ede9e5ad18e8553b5b6fcc2dbe0bf
Vonfry <mail@vonfry.name>
2019-10-15 16:46:15 +0800

    emacs: org file restruct
    reference: https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html

--- emacs.d/modules/lang/org/config.el
+++ emacs.d/modules/lang/org/config.el
@@ -6,44 +6,37 @@
    "org clock save")
 
 (defconst +org-todo-keywords-sequence
-  '((sequence "TODO(t)" "|" "DONE(d)" "REJECT(j)")
+  '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")
     (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)"))
   "org todo keywords")
-
-(defconst +org-capture-templates
-  '(
-    ("s" "shorts-todo" entry (file+headline "~/share/all/org-mode/misc.org" "shorts")
-     "* NEXT %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("e" "Event" entry (file+headline "~/share/all/org-mode/misc.org" "Events")
-     "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("i" "IST Templates")
-    ("is" "IST shorts" entry (file+headline "~/share/all/org-mode/IST.org" "shorts")
-     "* NEXT %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("ie" "IST event" entry (file+headline "~/share/all/org-mode/IST.org" "Events")
-     "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("ii" "IST isst" entry (file+headline "~/share/all/org-mode/IST.org" "Events")
-     "* %? IST isst: \n:PROPERTIES:\n:CREATED: %U\n:END:\n\n- [[contact:Ingo Pill][Ingo Pill]]\n- [[contact:Thomas Quartisch][Thomas Quartisch]]\n\n" :empty-lines 1)
-    ("b" "Besorgung" entry (file+headline "~/share/all/org-mode/hardware.org" "Besorgungen")
-     "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("C" "Clipboard" entry (file+headline "~/share/all/org-mode/misc.org" "shorts")
-     "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n%x\n\n" :empty-lines 1)
-    ("c" "capture to inbox, refile later" entry (file "~/share/all/org-mode/inbox.org")
-     "\n* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("m" "movie" entry (file+headline "~/share/all/org-mode/movies.org" "inbox")
-     "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-    ("x" "xlog")
-    ("xh" "xlog hometrainer" table-line (id "xlog-hometrainer") "| %T |  |  |  |")
-    ("xk" "Keyboard Akkus leer" table-line (id "3407c9b7-1b41-443b-9254-32c4af3a54e8") "| %T |")
-    )
-  "org capture templates")
 
 (defconst +org-capture-file
    (expand-file-name "inbox.org" vonfry-org-dir)
    "org capture")
 
+(defconst +org-capture-templates
+  '(("c" "capture to inbox, refile later" entry (file+headline +org-capture-file "Tasks")
+     "\n* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1))
+  "org capture templates")
+
 (defconst +org-brains-path
   (expand-file-name "brains" vonfry-org-dir)
   "org-brain path")
 
-(defconst +org-agenda-files (list +org-capture-file (expand-file-name "agenda/" vonfry-org-dir))
-  "global agenda dir")
+(let ((agenda-dir-with (lambda (p) (expand-file-name p (expand-file-name "agenda/" vonfry-org-dir)))))
+  (defconst +org-agenda-files
+    (list +org-capture-file
+          (agenda-dir-with "gtd.org")
+          (agenda-dir-with "someday.org"))
+    "global agenda dir")
+    (defconst +org-refile-targets
+      '(((agenda-dir-with "gtd.org") :maxlevel . 3)
+        ((agenda-dir-with "someday.org") :level . 1)))
+)
+
+(defconst +org-agenda-custom-commands
+  '((":d" "Categoriy: dev" tags-todo "dev"
+     ((org-agenda-overriding-header "Dev")))
+    ("@hs" "Context: haskell" tags-todo "@haskell"
+         ((org-agenda-overriding-header "Haskell")))))
+
