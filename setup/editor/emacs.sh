f01f1f024bd20b047cfd6706103544f29a3c56ee
Vonfry <mail@vonfry.name>
2019-10-16 10:25:27 +0800

    move clouddisk dir file

--- setup/editor/emacs.sh
+++ setup/editor/emacs.sh
@@ -1,4 +1,4 @@
 echo_info "** emacs"
-ln -f -s $script_dir/emacs.d ~/.emacs.d
-ln -f -s $clouddisk_dir/org ~/.org
+ln -f -s $script_dir/dotfiles/emacs.d ~/.emacs.d
+ln -f -s $clouddisk_dir/dotfiles/org ~/.org
 emacs --batch --load $script_dir/emacs.d/init.el --eval "(vonfry/compile-init-files)"
--- setup/shell/email.sh
+++ setup/shell/email.sh
@@ -3,11 +3,10 @@ echo_info "** set mutt"
 if [ $(uname) = "Linux" ]
     mkdir ~/.mutt 2> /dev/null
     ln -sf $script_dir/mutt/* ~/.mutt/
-    ln -sf $clouddisk_dir/mutt/* ~/.mutt/
-    ln -sf $clouddisk_dir/offlineimaprc ~/.offlineimaprc
-    ln -sf $clouddisk_dir/msmtprc ~/.msmtprc
+    ln -sf $clouddisk_dir/dotfiles/mutt/* ~/.mutt/
+    ln -sf $clouddisk_dir/dotfiles/offlineimaprc ~/.offlineimaprc
     mkdir -p ~/.local/passwd
-    ln -sf $clouddisk_dir/config/offlineimap.py ~/.config/offlineimap.py
+    ln -sf $clouddisk_dir/dotfiles/config/offlineimap.py ~/.config/offlineimap.py
 fi
 
 echo "--- Email client(mutt) config: please edit ~/.muttrc by yourself."
