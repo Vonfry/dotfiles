4f386bf14c915b675775709e17d649631f4e98eb
Vonfry <mail@vonfry.name>
2019-10-16 12:56:13 +0800

    xmonad: remove unused import

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -10,7 +10,6 @@ import XMonad.Prompt.XMonad
 import XMonad.Prompt.Man
 import XMonad.Layout.ShowWName
 import XMonad.Layout.Grid
-import XMonad.Layout.Accordion
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Hidden
 import XMonad.Layout.NoBorders
