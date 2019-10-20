bb6d854db52fab594fc6b667f4d5f095906e9b5a
Vonfry <mail@vonfry.name>
2019-10-20 08:42:03 +0800

    update script: output format

--- config/shell/update/pkgs/ruby.sh
+++ config/shell/update/pkgs/ruby.sh
@@ -3,8 +3,8 @@ if [ $(uname) = "Darwin" ]; then
     GEM="/usr/local/opt/ruby/bin/gem"
 fi
 if command -v gem > /dev/null 2>&1; then
-    echo -e "\n${ECHO_SYM}* ${ECHO_MSG}ruby${ECHO_RST}"
-    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}gem${ECHO_RST}\n"
+    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}ruby${ECHO_RST}"
+    echo -e "\n${ECHO_SYM}*** ${ECHO_MSG}gem${ECHO_RST}\n"
     $GEM update --user-install --quiet && $GEM update --system --quiet
 fi
 unset GEM
--- config/shell/update/shell.sh
+++ config/shell/update/shell.sh
@@ -1,4 +1,5 @@
+echo -e "\n${ECHO_SYM}* ${ECHO_MSG}shell${ECHO_RST}\n"
 if command -v zplug >/dev/null 2>&1; then
-    echo -e "\n${ECHO_SYM}* ${ECHO_MSG}zplug${ECHO_RST}\n"
+    echo -e "\n${ECHO_SYM}** ${ECHO_MSG}zplug${ECHO_RST}\n"
     zplug update
 fi
