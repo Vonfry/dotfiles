36a324ee35dacea83f6bded8ecdbf736a46c9087
Vonfry <mail@vonfry.name>
2019-10-25 15:28:43 +0800

    nixos: shell check profiles

--- config/shell/misc/nix.sh
+++ config/shell/misc/nix.sh
@@ -1,6 +1,10 @@
 is_eval_nix_path=0
 if [ $ID == nixos ]; then
     is_eval_nix_path=1
+    if [[ ! $(ls -al ~/.nix-profile) =~ /nix/var/nix/profiles/per-users/$USER/profiles ]]; then
+        rm ~/.nix-profile
+        nix-env --switch-profile /nix/var/nix/profiles/per-user/$USER/profile
+    fi
 fi
 if [ is_eval_nix_path ]; then
     export NIX_PATH=$NIX_PATH:nixpkgs-overlays=$DOTFILES_DIR/etc/nixos/user/vonfry/packages/custom/overlays.nix
