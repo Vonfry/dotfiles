2d4bdad29495903649d6a611320797effc644991
Vonfry <mail@vonfry.name>
2019-11-02 16:42:04 +0800

    nixos: font name

--- etc/nixos/base/font.nix
+++ etc/nixos/base/font.nix
@@ -10,7 +10,7 @@
     fontconfig = {
       enable = true;
       defaultFonts = {
-        monospace = [ "Hack" "Fira Code" ];
+        monospace = [ "Hack" "Fira Code Symbol" ];
 	    sansSerif = [ "Source Han Sans CN" ];
 	    serif = [];
       };
