f909b5a126c460a17826e998b529cb42a5718ba0
Vonfry <mail@vonfry.name>
2019-11-11 15:37:56 +0800

    zplug use

--- config/shell/zplug.zsh
+++ config/shell/zplug.zsh
@@ -3,8 +3,8 @@ source $ZPLUG_HOME/init.zsh
 zplug "Vonfry/prezto", at:revision/vonfry
 
 zplug "paulirish/git-open"
-zplug "mafredri/zsh-async", use:"async.zsh"
-zplug "b4b4r07/enhancd",    use:"init.sh"
+zplug "mafredri/zsh-async", use:async.zsh
+zplug "b4b4r07/enhancd",    use:init.sh
 zplug "MichaelAquilina/zsh-you-should-use"
 
 export GEOMETRY_PROMPT_PLUGINS=(exec_time jobs git hg kube)
