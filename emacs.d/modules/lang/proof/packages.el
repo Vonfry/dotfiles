fcaa50388aa6dd36bcc09322f30f1a9ab7868051
Vonfry <mail@vonfry.name>
2019-11-16 15:47:48 +0800

    emacs: proof with keybind fix and company mode

--- emacs.d/modules/lang/proof/packages.el
+++ emacs.d/modules/lang/proof/packages.el
@@ -5,25 +5,33 @@
   :general
   (nmap :prefix +nmap-lang-prefix
     :keymaps 'coq-mode-map
-    "<RET>" 'proof-goto-point
-    "b" 'proof-process-buffer
-    "r" 'proof-retract-buffer
-    "$" 'proof-shell-start
-    "x" 'proof-shell-exit
-    "p" 'proof-prf
-    "t" 'proof-ctxt
-    "h" 'proof-help
-    "`" 'proof-next-error
-    "f" 'proof-find-theorems
-    "v" 'proof-minibuffer-cmd
-    "b" 'proof-toolbar-toggle
-    ">" 'proof-autosend-toggle
-    "c" 'proof-interrupt-process
-    "d" 'proof-tree-external-display-toggle
-    "s" 'proof-toggle-active-scripting
-    "<BK>" 'proof-undo-and-delete-last-successful-command
-    "o" 'proof-display-some-buffers
-    "w" 'pg-response-clear-displays
-    "l" 'proof-layout-windows
-    "u" 'proof-undo-last-successful-command
-    "n" 'proof-assert-next-command-interactive))
+    "RET"    'proof-goto-point
+    "b"      'proof-process-buffer
+    "r"      'proof-retract-buffer
+    "$"      'proof-shell-start
+    "x"      'proof-shell-exit
+    "p"      'proof-prf
+    "t"      'proof-ctxt
+    "h"      'proof-help
+    "`"      'proof-next-error
+    "f"      'proof-find-theorems
+    "v"      'proof-minibuffer-cmd
+    "b"      'proof-toolbar-toggle
+    ">"      'proof-autosend-toggle
+    "c"      'proof-interrupt-process
+    "d"      'proof-tree-external-display-toggle
+    "s"      'proof-toggle-active-scripting
+    "DEL"    'proof-undo-and-delete-last-successful-command
+    "o"      'proof-display-some-buffers
+    "w"      'pg-response-clear-displays
+    "l"      'proof-layout-windows
+    "u"      'proof-undo-last-successful-command
+    "n"      'proof-assert-next-command-interactive))
+
+(package! company-coq
+  :after proof-general company
+  :hook coq-mode
+  :general
+  (nmap :prefix +nmap-lang-prefix
+        :keymaps 'coq-mode-map
+        "B" 'company-coq-cite))
