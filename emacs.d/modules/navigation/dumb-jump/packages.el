0b3dfba272818200b07dd88a6fafc9097edac3f6
Vonfry <mail@vonfry.name>
2019-11-17 19:12:13 +0800

    emacs: keybind fix

--- emacs.d/modules/navigation/dumb-jump/packages.el
+++ emacs.d/modules/navigation/dumb-jump/packages.el
@@ -3,20 +3,19 @@
 
 (package! dumb-jump
   :general
-  (nmap :prefix +nmap-leader
-        +nmap-go        'dumb-jump-go
-        +nmap-go-back   'dumb-jump-back
-        +nmap-go-peek   'dumb-jump-quick-look
-        +nmap-go-prompt 'dumb-jump-go-prompt)
-  (nmap :prefix +dumb-jump-nmap-prefix
-        :keymaps 'prog-mode-map
-        "" '(nil :which-key "dumb-jump")
-        +nmap-go        'dumb-jump-go
-        +nmap-go-back   'dumb-jump-back
-        +nmap-go-prompt 'dumb-jump-go-prompt
-        +nmap-go-peek   'dumb-jump-go-other-window
-        ">" 'dumb-jump-quick-look
-        "<" 'dumb-jump-go-prefer-external
-        ":" 'dumb-jump-go-prefer-external-other-window)
+  (+nmap-leader-def
+    :keymaps 'prog-mode-map
+    "."   'dumb-jump-go
+    ","   'dumb-jump-back
+    "&"   'dumb-jump-quick-look
+    "["   'dumb-jump-go-prompt)
+    ">"   '(nil :which-key "dumb-jump")
+    "> ." 'dumb-jump-go
+    "> ," 'dumb-jump-back
+    "> [" 'dumb-jump-go-prompt
+    "> ;" 'dumb-jump-go-other-window
+    "> >" 'dumb-jump-quick-look
+    "> <" 'dumb-jump-go-prefer-external
+    "> :" 'dumb-jump-go-prefer-external-other-window)
   :custom
   (dumb-jump-selector 'ivy))
