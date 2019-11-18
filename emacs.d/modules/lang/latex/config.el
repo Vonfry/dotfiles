4378b66810c182e6dc022fe908b1649e80883c03
Vonfry <mail@vonfry.name>
2019-11-18 14:08:16 +0800

    emacs: bugfix

--- emacs.d/modules/lang/latex/config.el
+++ emacs.d/modules/lang/latex/config.el
@@ -1,8 +1,6 @@
 ;;; latex config -*- lexical-binding: t -*-
 ;;
 
-(defconst +tex-keymaps )
-
 ;; +nmap-lang-tex-def
 (vonfry-def-nmap-lang-prefix tex nil
   :keymaps '(LaTeX-mode-map tex-mode-map))
