8234de5bb6de2b4bfef03987c9121f2f1b81e95c
Vonfry <mail@vonfry.name>
2019-11-20 10:20:09 +0800

    eamcs: pretty hook change

--- emacs.d/modules/visual/pretty/packages.el
+++ emacs.d/modules/visual/pretty/packages.el
@@ -3,7 +3,7 @@
 
 (add-hook 'after-change-major-mode-hook #'+pretty-code-init-pretty-symbols-h)
 
-(add-hook 'doom-init-ui-hook #'+pretty-code-setup-fira-ligatures-h)
+(add-hook 'window-setup-hook #'+pretty-code-setup-fira-ligatures-h)
 
 (custom-set-variables
   '(prettify-symbols-unprettify-at-point 'right-edge))
