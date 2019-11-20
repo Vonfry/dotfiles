c941c8977d75ab876a5fe8215b0e85042f698457
Vonfry <mail@vonfry.name>
2019-11-20 12:53:30 +0800

    latex: latexmkrc fix

--- setup/shell/latexmk.sh
+++ setup/shell/latexmk.sh
@@ -2,17 +2,17 @@ if [ $(uname) = "linux" ]; then
 
 cat <<EOF > ~/.latexmkrc
 
-$dvi_previewer = 'xdvi -watchfile 1.5';
-$ps_previewer  = 'feh';
-$pdf_previewer = 'qpdfviewer';
+\$dvi_previewer = 'xdvi -watchfile 1.5';
+\$ps_previewer  = 'feh';
+\$pdf_previewer = 'qpdfviewer';
 EOF
 
 elif [ $(uname) = 'darwin' ]; then
 
 cat <<EOF > ~/.latexmkrc
 
-$pdf_previewer = 'open -a Skim';
-$pdflatex = 'pdflatex -synctex=1 -interaction=nonstopmode';
+\$pdf_previewer = 'open -a Skim';
+\$pdflatex = 'pdflatex -synctex=1 -interaction=nonstopmode';
 @generated_exts = (@generated_exts, 'synctex.gz');
 EOF
 
