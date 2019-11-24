fa68ff431a1bfe8fc01b42337b59c7503ab3d702
Vonfry <mail@vonfry.name>
2019-11-24 17:20:12 +0800

    update: fix rust file path

--- config/shell/update/pkgs/rust.sh
+++ config/shell/update/pkgs/rust.sh
@@ -1,3 +1,3 @@
 echo -e "\n${ECHO_SYM}** ${ECHO_MSG}rust${ECHO_RST}"
 echo -e "\n${ECHO_SYM}*** ${ECHO_MSG}rustup${ECHO_RST}"
-cat $DOTFILES_DIR/config/pkgs/rust.txt | xargs -n1 rustup component add rls
+cat $DOTFILES_DIR/config/pkgs/rustup.txt | xargs -n1 rustup component add rls
