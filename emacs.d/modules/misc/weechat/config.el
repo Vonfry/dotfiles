2d867e5c450b66a286941effba4a82479757672b
Vonfry <mail@vonfry.name>
2019-11-26 09:27:17 +0800

    emacs: weechat config

--- emacs.d/modules/misc/weechat/config.el
+++ emacs.d/modules/misc/weechat/config.el
@@ -1,2 +1,11 @@
 ;;; weechat config -*- lexical-binding: t -*-
 ;;
+
+(defcustom +weechat-local-config-file
+  (let* ((cloud-path (getenv "CLOUDDISK_DIR"))
+         (file (expand-file-name "weechat" cloud-path)))
+    (if (file-exists-p file)
+        cloud-file-path
+      nil))
+  "weechat private config file."
+  :group 'vonfry-module)
