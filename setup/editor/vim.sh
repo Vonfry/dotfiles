a75ffd550310c3c02c78001150a19737a3a20e63
Vonfry <mail@vonfry.name>
2019-11-27 12:42:15 +0800

    setup: fix a vim ln

--- setup/editor/vim.sh
+++ setup/editor/vim.sh
@@ -2,4 +2,4 @@ echo_info "** vim"
 ln -f -s $script_dir/vimrc ~/.vimrc
 mkdir -p ~/.cache/vim
 mkdir -p ~/.cache/vim/undodir
-echo "vim nvim pkgs shell" | xargs -n1 -I "{}" ln -f -s $script_dir/config/{} ~/.config/
+echo "nvim pkgs shell" | xargs -n1 -I "{}" ln -f -s $script_dir/config/{} ~/.config/
