ba47b870cedbfa1563c3c27a85e47646ebe209e9
Vonfry <mail@vonfry.name>
2019-12-06 10:28:29 +0800

    Revert "emacs: readd fira code symbols"
    This reverts commit 8e2dc8e2b0f9ed904bd16714a252902f3889abf3.

---
+++ emacs.d/modules/visual/pretty/packages.el
@@ -0,0 +1,9 @@
+;;; pretty modules -*- lexical-binding: t; -*-
+;;
+
+; (add-hook 'prog-mode-hook #'+pretty-code-init-pretty-symbols-h)
+
+(add-hook 'window-setup-hook #'+pretty-code-setup-h)
+
+(custom-set-variables
+  '(prettify-symbols-unprettify-at-point 'right-edge))
