c5ae9180e299630bd3e2bf7f8f35c1209a74f5d8
Vonfry <mail@vonfry.name>
2019-12-08 13:03:29 +0800

    nixos: merge script fix

--- config/shell/misc/nixos.sh
+++ config/shell/misc/nixos.sh
@@ -4,7 +4,22 @@ if [ -f /etc/NIXOS ]; then
     {
         (
         cd /etc/nixos
-        sudo diff -rud . $DOTFILES_DIR/etc/nixos | sudo patch -p0
+        ignore_files=(
+            hardware-configuration.nix
+            local
+            \*.local.\*
+            .bundle
+            Gemfile.lock
+            gemset.nix
+            readme.\*
+            .gitignore
+        )
+        ignore_command=""
+        for f in "${ignore_files[@]}"; do
+            ignore_command="$ignore_command -x $f"
+        done
+        eval sudo diff -ruNd  $ignore_command . $DOTFILES_DIR/etc/nixos \
+            | sudo patch -b -p0
         sudo nixos-rebuild switch
         )
     }
