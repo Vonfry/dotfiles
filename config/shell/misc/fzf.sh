cd6c47e772468f4fa5f51d7eb5ad27200a5c2bc1
Vonfry <mail@vonfry.name>
2019-12-09 18:52:55 +0800

    nixos: merge command

--- config/shell/misc/fzf.sh
+++ config/shell/misc/fzf.sh
@@ -1,20 +1,9 @@
-if [ -n "${BASH}" ]; then
-    shell="bash"
-elif [ -n "${ZSH_NAME}" ]; then
-    shell="zsh"
-elif [ -n "${__fish_datadir}" ]; then
-    shell="fish"
-elif [ -n "${version}" ]; then
-    shell="tcsh"
-else
-    shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
-fi
 export FZF_DEFAULT_COMMAND='fd'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 if   [ -f ~/.fzf.zsh ]; then
     source ~/.fzf.zsh
 elif [ $ID == nixos ]; then
-    for f in $(nix-store --quiet --quiet -r $(which fzf))/share/fzf/*.$shell; do
+    for f in $(nix-store --quiet --quiet -r $(which fzf))/share/fzf/*.$SHELL_NAME; do
         source $f
     done
 fi
