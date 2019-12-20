b6c4eefaab5d735d849e9e18cab2dac99ad76648
Vonfry <mail@vonfry.name>
2019-12-20 16:25:48 +0800

    emacs: weechat add some default value

--- emacs.d/modules/misc/weechat/packages.el
+++ emacs.d/modules/misc/weechat/packages.el
@@ -4,6 +4,8 @@
 (package! weechat
   :custom
   (weechat-mode-default 'ssl)
+  (weechat-host-default "chat.freenode.org")
+  (weechat-port-default 6697)
   :config
   (if +weechat-local-config-file
     (load +weechat-local-config-file t t)
