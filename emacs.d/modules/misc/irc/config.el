4fb57eea7a20a74b7ad7f01c6d021d7cc5d35cf8
Vonfry <mail@vonfry.name>
2019-12-22 09:27:03 +0800

    emacs: remove irc custom file, use private modules instead

--- emacs.d/modules/misc/irc/config.el
+++ emacs.d/modules/misc/irc/config.el
@@ -1,7 +1,2 @@
 ;;; irc config -*- lexical-binding: t -*-
 ;;
-
-(defcustom +irc-local-config-file
-  (expand-file-name "dotfiles/emacs/irc" (getenv "CLOUDDISK_DIR"))
-  "irc private config file."
-  :group 'vonfry-module)
--- setup/shell/email.sh
+++ setup/shell/email.sh
@@ -3,6 +3,8 @@ echo_info "** email"
 if [ $(uname) = "Linux" ]; then
     mkdir -p ~/.local/passwd
     mkdir -p ~/.mail
+    ln -s $clouddisk_dir/dotfiles/fetchmailrc ~/.fetchmailrc
+    ln -s $clouddisk_dir/dotfiles/emacs.d/mail $script_dir/emacs.d/modules/private/mail
+    ln -s $clouddisk_dir/dotfiles/emacs.d/irc $script_dir/emacs.d/modules/private/irc
+    echo_note "--- Please create netrc file to set password."
 fi
-
-echo "--- Email client(mutt) config: please edit ~/.muttrc by yourself."
