e4781e4c65c0f5b6228c58190355509b01c6c37e
Vonfry <mail@vonfry.name>
2019-12-23 15:27:26 +0800

    emacs: remave unused tools and add elfeed tools

--- emacs.d/modules/misc/elfeed/packages.el
+++ emacs.d/modules/misc/elfeed/packages.el
@@ -1,7 +1,15 @@
 ;;; elfeed packages -*- lexical-binding: t -*-
 ;;
 
+;(defun +elfeed-general-build-hook (pkg &rest args)
+;  (when (string= pkg "elfeed")
+;    (straight--fix-org-function "org")))
+;(add-to-list 'straight-use-package-prepare-functions #'+elfeed-general-build-hook)
 (package! elfeed
+  :hook
+  (evil-mode . (lambda ()
+    (evil-set-initial-state 'elfeed-search-mode 'emacs)
+    (evil-set-initial-state 'elfeed-show-mode   'emacs)))
   :general
   ("C-x w" 'elfeed)
   (+mmap-at-def
