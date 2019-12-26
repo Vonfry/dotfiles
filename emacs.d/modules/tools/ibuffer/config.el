35565426deb7276b0f7036ffcdac7b1e9634501b
Vonfry <mail@vonfry.name>
2019-12-26 13:07:46 +0800

    emacs: ibuferr & irc change.

--- emacs.d/modules/tools/ibuffer/config.el
+++ emacs.d/modules/tools/ibuffer/config.el
@@ -4,6 +4,17 @@
 (defcustom +ibuffer-filter-groups
   '(("Circe" (or (mode . circe-channel-mode)
                  (mode . circe-query-mode)
-                 (mode . circe-server-mode))))
+                 (mode . circe-server-mode)))
+    ("Mu4e" (starred-name . "*mu4e-"))
+    ("Flycheck" (starred-name . "*Flycheck"))
+    ("Elfeed" (starred-name . "*elfeed"))
+    ("Process" (or (mode . process-menu-mode)
+                   (mode . proced-mode)))
+    ("Lisp" (or (mode . lisp-interaction-mode)))
+    ("LSP" (starred-name . "*lsp-"))
+    ("Message" (or (starred-name . "*straight")
+                   (starred-name . "*Messages*")
+                   (starred-name . "*direnv*")
+                   (mode         . help-mode))))
   "ibuffer groups, because of `ibuffer-projectile', config it here. See more `ibuffer-filter-groups'"
   :group 'vonfry-modules)
