764ff496014db115c22d697147c86c19612ba0fc
Vonfry <mail@vonfry.name>
2019-12-27 12:24:08 +0800

    emacs: remove googler

--- setup/os/macos.sh
+++ setup/os/macos.sh
@@ -17,7 +17,6 @@ brew cask install alacritty
 brew reinstall fzf && /usr/local/opt/fzf/install
 brew reinstall dbus
 brew reinstall thefuck
-brew reinstall googler
 # brew reinstall htop
 brew reinstall lolcat                        # colorful print
 brew reinstall tree                          # file browser
