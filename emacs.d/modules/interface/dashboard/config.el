9d5f85fce6258893c46096597df0a0ee6e5b6e0f
Vonfry <mail@vonfry.name>
2019-12-29 16:23:38 +0800

    emacs: dashboard custom

--- emacs.d/modules/interface/dashboard/config.el
+++ emacs.d/modules/interface/dashboard/config.el
@@ -1,2 +1,8 @@
 ;;; dashboard config -*- lexical-binding: t -*-
 ;;
+
+(custom! +dashboard-banner (if (file-exists-p "~/.config/bg.png") "~/.config/bg.png" 'logo)
+  ""
+  :type 'sexp
+  :group 'vonfry-modules
+  :custom-set 'dashboard-startup-banner)
--- emacs.d/modules/interface/dashboard/packages.el
+++ emacs.d/modules/interface/dashboard/packages.el
@@ -4,16 +4,42 @@
 (package! dashboard
   :custom
   (dashboard-center-content t)
-  (dashboard-startup-banner "~/.config/bg.png")
+  (dashboard-startup-banner +dashboard-banner)
   (initial-buffer-choice '+dashboard-goto)
+  (dashboard-set-init-info nil)
   (dashboard-banner-logo-title "Happy Hacking!")
   (dashboard-show-shortcuts    t)
+  (dashboard-set-navigator     t)
   (dashboard-set-heading-icons t)
   (dashboard-set-file-icons    t)
   (dashboard-items '((agenda    . 5)
                      (bookmarks . 5)
                      (projects  . 5)
                      (recents   . 5)))
+  (dashboard-navigator-buttons
+      `(;; (icon title help action face prefix suffix)
+        ((,(all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0)
+           "Mail"
+           "Check Mail"
+           (lambda (&rest _) (mu4e)))
+        (,(all-the-icons-material "chat" :height 1.0 :v-adjust 0.0)
+          "Freenode"
+          "Freenode"
+          (lambda (&rest _) (circe "Freenode")))
+        (,(all-the-icons-material "chat" :height 1.0 :v-adjust 0.0)
+          "Gist"
+          "Gist"
+          (lambda (&rest _) (circe "Gist"))))
+        ((,(all-the-icons-fileicon "gitlab" :height 1.0 :v-adjust 0.0)
+           "Gitlab"
+           "Goto Gitlab"
+           (lambda (&rest _) (browse-url "https://gitlab.com/Vonfry")))
+         (,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
+           "Github"
+           "Goto Github"
+           (lambda (&rest _) (browse-url "https://github.com/Vonfry"))))))
+  :hook
+  (dashboard-mode . (lambda () (custom-set-variables '(show-trailing-whitespace nil))))
   :config
   (dashboard-setup-startup-hook)
   :general
