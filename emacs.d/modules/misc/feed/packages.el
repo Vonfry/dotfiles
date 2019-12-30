f2b4fc971fdfbf82c299c378d9262a4edc38b85d
Vonfry <mail@vonfry.name>
2019-12-30 11:47:21 +0800

    emacs: fix trailing

--- emacs.d/modules/misc/feed/packages.el
+++ emacs.d/modules/misc/feed/packages.el
@@ -6,9 +6,10 @@
   (elfeed-db-directory (expand-file-name "elfeed/db" vonfry-local-dir))
   (elfeed-enclosure-directory (expand-file-name "elfeed/enclosure" vonfry-local-dir))
   :hook
-  (evil-mode . (lambda ()
+  ((evil-mode . (lambda ()
     (evil-set-initial-state 'elfeed-search-mode 'emacs)
     (evil-set-initial-state 'elfeed-show-mode   'emacs)))
+   ((elfeed-show elfeed-search) . (lambda () (custom-set-variables '(show-trailing-whitespace nil)))))
   :general
   ("C-x w" 'elfeed)
   (+mmap-at-def
