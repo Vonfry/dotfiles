9f1e3622b07eced8653a5cad3accd736da730413
Vonfry <mail@vonfry.name>
2019-12-30 09:39:41 +0800

    remote autojump

--- config/shell/misc/autojump.sh
+++
@@ -1,5 +0,0 @@
-if [ -f /usr/local/etc/profile.d/autojump.sh ]; then
-    source /usr/local/etc/profile.d/autojump.sh
-elif [ "$ID" = nixos ]; then
-    source $(nix-store --quiet --quiet -r $(which autojump))/share/autojump/autojump.$SHELL_NAME
-fi
