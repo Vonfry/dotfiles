49454e59fa9eb86e53533dbff861f4793f7fc4fb
Vonfry <mail@vonfry.name>
2019-12-30 09:25:12 +0800

    emacs: change cloud action

--- emacs.d/modules/misc/cloud/config.el
+++ emacs.d/modules/misc/cloud/config.el
@@ -2,7 +2,13 @@
 ;;
 
 (custom! +cloud-diskdir
-  (getenv "CLOUDDISK_DIR")
+  (expand-file-name "dotfiles" (getenv "CLOUDDISK_DIR"))
+  "where is your cloud dir syncing by commands"
+  :type 'directory
+  :group 'vonfry-modules)
+
+(custom! +cloud-remotedir
+  "cloud:cfg/dotfiles"
   "where is your cloud dir syncing by commands"
   :type 'directory
   :group 'vonfry-modules)
@@ -42,22 +48,22 @@
   :type 'string
   :group 'vonfry-modules)
 
-(custom! +cloud-push-argument (s-join " " (list +cloud-diskdir "cloud:cfg"))
+(custom! +cloud-push-argument (s-join " " (list +cloud-diskdir +cloud-remote-dir))
   "the push argument "
   :type 'string
   :group 'vonfry-modules)
 
-(custom! +cloud-get-argument (s-join " " (list "cloud:cfg" +cloud-diskdir))
+(custom! +cloud-get-argument (s-join " " (list +cloud-remote-dir +cloud-diskdir))
   "the get argument "
   :type 'string
   :group 'vonfry-modules)
 
-(custom! +cloud-copy-to-argument (s-join " " (list +cloud-diskdir "cloud:cfg"))
+(custom! +cloud-copy-to-argument (s-join " " (list +cloud-diskdir +cloud-remote-dir))
   "the copy to argument "
   :type 'string
   :group 'vonfry-modules)
 
-(custom! +cloud-copy-from-argument (s-join " " (list "cloud:cfg" +cloud-diskdir))
+(custom! +cloud-copy-from-argument (s-join " " (list +cloud-remote-dir +cloud-diskdir))
   "the copy from argument "
   :type 'string
   :group 'vonfry-modules)
