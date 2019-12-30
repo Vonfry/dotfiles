d288e7230fae65ac7faae707c00e413ebb7dfb81
Vonfry <mail@vonfry.name>
2019-12-30 10:22:11 +0800

    emacs: style

--- emacs.d/modules/misc/mail/packages.el
+++ emacs.d/modules/misc/mail/packages.el
@@ -53,7 +53,7 @@
     (lambda ()
       (mu4e-alert-enable-mode-line-display)
       (vonfry-system-sets (gnu/linux (mu4e-alert-set-default-style 'libnotify))
-                          (darwin (mu4e-alert-set-default-style 'growl)))
+                          (darwin    (mu4e-alert-set-default-style 'growl)))
       (mu4e-alert-enable-notifications))))
 
 (package! mu4e-maildirs-extension
