bb7ea4e659e3edc17d90c666a14b1d26b9e0b4a5
Vonfry <mail@vonfry.name>
2020-05-06 10:59:38 +0800

    shell:  source nix

--- config/shell/misc/nix.sh
+++ config/shell/misc/nix.sh
@@ -1,7 +1,7 @@
 is_eval_nix_path=0
 if [[ -n $ID && $ID == nixos ]]; then
     is_eval_nix_path=1
-    if [[ ! $(ls -al ~/.nix-profile) =~ /nix/var/nix/profiles/per-users/$USER/profiles ]]; then
+    if [[ ! $(ls -al ~/.nix-profile) =~ /nix/var/nix/profiles/per-users/$USER/profile ]]; then
         rm ~/.nix-profile
         nix-env --switch-profile /nix/var/nix/profiles/per-user/$USER/profile
     fi
