18eaf8660b11f200caa4ee9f4f0ba4a3e4147707
Vonfry <mail@vonfry.name>
2020-05-21 14:02:54 +0800

    emacs: add doc string

--- config/emacs.d/modules/misc/bib/config.el
+++ config/emacs.d/modules/misc/bib/config.el
@@ -2,18 +2,20 @@
 ;;
 
 (fun! +bib--set-dir (name val)
-  "set bib directory"
+  "set bib directory."
   (set-default name val)
   (when val
     (custom-set! ebib-notes-directory val)
                 +bib-search-dir (add-to-list '+bib-search-dir val)))
 
 (custom! +bib-dir nil
+  "bib directory. the bibs in this dir and notes under the same one."
  :group 'vonfry-modules
  :type 'directory
  :set #'+bib--set-dir)
 
 (custom! +bib-search-dir nil
+  "bib search dir."
   :group 'vonfry-modules
   :type '(repeat 'directory)
   :custom-set 'ebib-bib-search-dirs)
