7a89fccb36a40fe135cdd33314bfd13537021f3e
Vonfry <mail@vonfry.name>
2020-05-27 15:30:08 +0800

    emacs: bib keybind

--- config/emacs.d/modules/misc/bib/packages.el
+++ config/emacs.d/modules/misc/bib/packages.el
@@ -6,7 +6,24 @@
   (ebib-filters-default-file (expand-file-name "ebib/filters" vonfry-cache-dir))
   (ebib-file-associations (vonfry-system-sets
                            (darwin '(("pdf" . "open")))
-                           (gnu/linux '(("pdf" . "zathura"))))))
+                           (gnu/linux '(("pdf" . "zathura")))))
+  :hook
+  (evil-mode . (lambda ()
+    (evil-set-initial-state 'ebib-index-mode  'emacs)
+    (evil-set-initial-state 'ebib-entry-mode  'emacs)
+    (evil-set-initial-state 'ebib-log-mode    'emacs)
+    (evil-set-initial-state 'ebib-string-mode 'emacs)))
+  :general
+  (+mmap-mode-org-def
+    "(" 'ebib
+    "*" '+bib/switch-dir
+    ")" '+bib/switch-dir-from-search-dir
+    "[" 'ebib-insert-citation)
+  (+mmap-mode-tex-def
+    "(" 'ebib
+    "*" '+bib/switch-dir
+    ")" '+bib/switch-dir-from-search-dir
+    "[" 'ebib-insert-citation))
 
 (package! ivy-bibtex
   :after ivy
@@ -15,6 +32,8 @@
   (bibtex-completion-library-path '("./refs"))
   :general
   (+mmap-mode-tex-def
+    "%" '+bib/switch-ref
     "&" 'ivy-bibtex)
   (+mmap-mode-org-def
+    "%" '+bib/switch-ref
     "&" 'ivy-bibtex))
