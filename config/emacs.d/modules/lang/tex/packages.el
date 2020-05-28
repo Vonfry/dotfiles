76dc05625c8904b24d6ca3c8bdccb42da302d12f
Vonfry <mail@vonfry.name>
2020-05-28 15:36:59 +0800

    emacs/tex: fix hook
    emacs/tex fix

--- config/emacs.d/modules/lang/tex/packages.el
+++ config/emacs.d/modules/lang/tex/packages.el
@@ -33,82 +33,75 @@
   (TeX-show-compilation t) ; display compilation windows
   (TeX-source-correlate-mode t)
   (TeX-source-correlate-method 'synctex)
+  (reftex-citation-format 'biblatex)
   :hook
-  ((TeX-mode . font-lock-mode)
-   (TeX-mode . LaTeX-mode)
-   (LaTeX-mode . LaTeX-math-mode)
-   (TeX-mode . TeX-source-correlate-mode)
-   (TeX-mode . TeX-PDF-mode)
-   (TeX-mode . TeX-fold-mode)
-   (TeX-after-compilation-finished-functions . TeX-revert-document-buffer)
-   (TeX-mode  .
-     (lambda()
+  (LaTeX-mode .
+     (lambda ()
        (custom-set! compile-command "latexmk")
        (require 'preview)
        (require 'tex-site)
        ;; use pdfview with auctex
        ;; have the buffer refresh after compilation
-       (turn-on-reftex)
-       (define-key TeX-mode-map (kbd "TAB") 'TeX-complete-symbol)
-       (imenu-add-menubar-index))))
+       (reftex-mode)
+       (rainbow-delimiters-mode)
+       (LaTeX-math-mode)))
   :general
+  (:keymaps 'TeX-mode-map
+    (kbd "TAB") 'TeX-complete-symbol)
   (+mmap-tex-def
     "?"   'TeX-doc
     "RET" 'preview-at-point
     "r"   'compile
     "R"   'TeX-command-run-all)
   (+mmap-mode-tex-def
-    "\\"  'TeX-insert-macro
-    "-"   'TeX-recenter-output-buffer
-    "%"   'TeX-comment-or-uncomment-paragraph
-    ";"   'TeX-comment-or-uncomment-region
-    "b"   'latex/build
-    "k"   'TeX-kill-job
-    "l"   'TeX-recenter-output-buffer
-    "m"   'TeX-insert-macro
-    "v"   'TeX-view
-    "C"   'TeX-clean
-    "n"   'TeX-narrow-to-group
-    "t"   '(nil :which-key "font")
-    "b"   'latex/font-bold
-    "t c" 'latex/font-code
-    "t e" 'latex/font-emphasis
-    "t i" 'latex/font-italic
-    "t u" 'latex/font-small-caps
-    "t f" 'latex/font-sans-serif
-    "t r" 'latex/font-serif
-    "r"   'latex/font-clear
-    "o"   'latex/font-oblique
-    "B"   'latex/font-medium
-    "r"   'latex/font-clear
-    "t a" 'latex/font-calligraphic
-    "t n" 'latex/font-normal
-    "t U" 'latex/font-upright
-    "*"   'LaTeX-mark-section
-    "."   'LaTeX-mark-environment
-    "c"   'LaTeX-close-environment
-    "e"   'LaTeX-environment
-    "i"   'LaTeX-insert-item
-    "s"   'LaTeX-section
-    "f"   '(nil :which-key "fill")
-    "f e" 'LaTeX-fill-environment
-    "f p" 'LaTeX-fill-paragraph
-    "f r" 'LaTeX-fill-region
-    "f s" 'LaTeX-fill-section
-    "p"   '(nil :which-key "preview")
-    "p b" 'preview-buffer
-    "p c" 'preview-clearout
-    "p d" 'preview-document
-    "p e" 'preview-environment
-    "p f" 'preview-cache-preamble
-    "p r" 'preview-region
-    "p s" 'preview-section
-    "z"   '(nil :which-key "fold")
-    "z =" 'TeX-fold-math
-    "z b" 'TeX-fold-buffer
-    "z e" 'TeX-fold-env
-    "z m" 'TeX-fold-macro
-    "z r" 'TeX-fold-region))
+    "\\"    'TeX-insert-macro
+    "-"     'TeX-recenter-output-buffer
+    "%"     'TeX-comment-or-uncomment-paragraph
+    ";"     'TeX-comment-or-uncomment-region
+    "k"     'TeX-kill-job
+    "l"     'TeX-recenter-output-buffer
+    "m"     'TeX-insert-macro
+    "v"     'TeX-view
+    "C"     'TeX-clean
+    "n"     'TeX-narrow-to-group
+    "*"     'LaTeX-mark-section
+    "."     'LaTeX-mark-environment
+    "c"     'LaTeX-close-environment
+    "e"     'LaTeX-environment
+    "i"     'LaTeX-insert-item
+    "s"     'LaTeX-section
+    "f"     '(nil :which-key "fill")
+    "f e"   'LaTeX-fill-environment
+    "f p"   'LaTeX-fill-paragraph
+    "f r"   'LaTeX-fill-region
+    "f s"   'LaTeX-fill-section
+    "p"     '(nil :which-key "preview")
+    "p b"   'preview-buffer
+    "p c"   'preview-clearout
+    "p d"   'preview-document
+    "p e"   'preview-environment
+    "p f"   'preview-cache-preamble
+    "p r"   'preview-region
+    "p s"   'preview-section
+    "z"     '(nil :which-key "fold")
+    "z ="   'TeX-fold-math
+    "z b"   'TeX-fold-buffer
+    "z e"   'TeX-fold-env
+    "z m"   'TeX-fold-macro
+    "z r"   'TeX-fold-region
+    "rc"    'reftex-citation
+    "rg"    'reftex-grep-document
+    "ri"    'reftex-index-selection-or-word
+    "rI"    'reftex-display-index
+    "r TAB" 'reftex-index
+    "rl"    'reftex-label
+    "rp"    'reftex-index-phrase-selection-or-word
+    "rP"    'reftex-index-visit-phrases-buffer
+    "rr"    'reftex-reference
+    "rs"    'reftex-search-document
+    "rt"    'reftex-toc
+    "rT"    'reftex-toc-recenter
+    "rv"    'reftex-view-crossref))
 
 (package! auctex-latexmk
   :after latex
