81a9f3f811fd0e71e96d9103d2088ef30a400079
Vonfry <mail@vonfry.name>
2020-06-16 09:26:36 +0800

    xmonad: use EZConfig and submap instead original keybind defining and change some keybind

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 
 import XMonad hiding ((|||))
+import XMonad.Util.EZConfig
 import XMonad.Actions.Search
 import XMonad.Actions.WindowMenu
-import XMonad.Actions.GridSelect -- TODO remove this if prompt work fine
 import XMonad.Layout.WorkspaceDir
 import XMonad.Actions.CycleWS
 import XMonad.Prompt
@@ -23,8 +23,7 @@ import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.Renamed
 import XMonad.Util.Paste
 import XMonad.Util.Run
-import qualified XMonad.StackSet as W
-import qualified  Data.Map as M
+import XMonad.StackSet
 import Data.Ratio
 import System.IO
 
@@ -46,7 +45,7 @@ myXPConf = def
     , bgHLight        = draculaSelection
     , fgHLight        = draculaForeground
     , borderColor     = draculaPuple
-    , autoComplete    = Just $ 5 * 10 ^ 5 -- 0.5s
+    , autoComplete    = Just $ 2 * 10 ^ 5 -- 0.2s
     }
 
 myGSConf = def
@@ -71,138 +70,120 @@ myApps =
     , ("video"     , "vlc"                   )
     ]
 
-data MyApps = MyApps
-
-instance XPrompt MyApps where
-  showXPrompt MyApps = "App: "
-
 -- my configurations
 
-myKeys conf@(XConfig {modMask = modm}) = M.fromList
-    [ ((modm              , xK_x    ), shellPrompt myXPConf)
-    , ((modm .|. shiftMask, xK_x    ), xmonadPrompt myXPConf)
-    , ((modm              , xK_slash), promptSearch myXPConf multi)
-    , ((modm              , xK_comma)
-      , runSelectedAction myGSConfS $
-        fmap (\(name, cmd) -> (name, spawn cmd)) myApps)
-    , ((modm .|. shiftMask, xK_comma)
-      , mkXPrompt MyApps myXPConf
-                  (mkComplFunFromList $ fst $ unzip myApps)
-                  ((\case Just cmd -> spawn cmd
-                          Nothing  -> return ()) . flip lookup $ myApps)) -- TODO test
+myKeys conf = mkKeymap conf
+    [ ("M-x"  , shellPrompt myXPConf)
+    , ("M-S-x", xmonadPrompt myXPConf)
+    , ("M-/"  , promptSearch myXPConf multi)
+
+    , ("M-, d" , "zeal"                  )
+    , ("M-, b" , "firefox"               )
+    , ("M-, B" , myCLI "w3m"             )
+    , ("M-, a" , myCLI "alsamixer"       )
+    , ("M-, D" , "libreoffice"           )
+    , ("M-, V" , "VirtualBox"            )
+    , ("M-, v" , "zathura"               )
+    , ("M-, t" , "telegram-desktop"      )
+    , ("M-, m" , "vlc"                   )
+    , ("M-, '" , spawn "emacs"           )
 
     -- basic window
-    , ((modm              , xK_apostrophe), spawn $ myCLI "ranger")
-    , ((modm .|. shiftMask, xK_apostrophe), spawn "emacs"         )
-    , ((modm .|. shiftMask, xK_Return    ), spawn $ XMonad.terminal conf)
-    , ((modm .|. shiftMask, xK_c         ), kill)
-    , ((modm .|. shiftMask, xK_space     ), setLayout $ XMonad.layoutHook conf)
-    , ((modm              , xK_n         ), refresh)
+    , ("M-'"         , spawn $ myCLI "ranger"            )
+    , ("M-S-<Return>", spawn $ XMonad.terminal conf      )
+    , ("M-S-c"       , kill                              )
+    , ("M-S-<Space>" , setLayout $ XMonad.layoutHook conf)
+    , ("M-n"         , refresh                           )
 
     -- move focus up or down the window stack
-    , ((modm              , xK_Tab   ), windows W.focusDown  )
-    , ((modm .|. shiftMask, xK_Tab   ), windows W.focusUp    )
-    , ((modm              , xK_m     ), windows W.focusMaster)
-    , ((modm              , xK_Return), windows W.swapMaster )
+    , ("M-<Tab>"   , windows focusDown  )
+    , ("M-S-<Tab>" , windows focusUp    )
+    , ("M-m"       , windows focusMaster)
+    , ("M-<Return>", windows swapMaster )
 
     -- resizing the master/slave ratio
-    , ((modm, xK_bracketleft ), sendMessage Shrink)
-    , ((modm, xK_bracketright), sendMessage Expand)
+    , ("M-(", sendMessage Shrink)
+    , ("M-)", sendMessage Expand)
 
     -- floating layer support
-    , ((modm              , xK_t), withFocused $ windows . W.sink)
-    , ((modm .|. shiftMask, xK_t), withFocused float)
+    , ("M-t"  , withFocused $ windows . sink)
+    , ("M-S-t", withFocused float                 )
 
     -- increase or decrease number of windows in the master area
-    , ((modm, xK_parenleft ), sendMessage (IncMasterN (-1)))
-    , ((modm, xK_parenright), sendMessage (IncMasterN 1))
+    , ("M-[", sendMessage (IncMasterN (-1)))
+    , ("M-]", sendMessage (IncMasterN 1   ))
 
     -- quit, or restart
-    , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
-    , ((modm              , xK_q), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
+    , ("M-S-q", io (exitWith ExitSuccess))
+    , ("M-q"  , spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
 
     -- help
-    , ((modm .|. shiftMask, xK_slash), helpCommand)
+    , ("M-?", helpCommand)
 
     -- screenshot
-    , ((modm                , xK_Print), spawn "flameshot gui    -p ~/screenshot/" )
-    , ((modm .|. controlMask, xK_Print), spawn "flameshot screen -p  ~/screenshot/")
-    , ((modm .|. shiftMask  , xK_Print), spawn "flameshot full   -p ~/screenshot"  )
+    , ("M-<Print>"  , spawn "flameshot gui    -p ~/screenshot/" )
+    , ("M-S-<Print>", spawn "flameshot screen -p ~/screenshot/" )
+    , ("M-C-<Print>", spawn "flameshot full   -p ~/screenshot"  )
 
     -- Switch between layers
-    , ((modm                , xK_space), switchLayer)
+    , ("M-<Space>", switchLayer)
 
-    -- Directional navigation of windows
-    , ((modm                , xK_l    ), windowGo R False)
-    , ((modm                , xK_h    ), windowGo L False)
-    , ((modm                , xK_j    ), windowGo U False)
-    , ((modm                , xK_k    ), windowGo D False)
-
-    -- Swap adjacent windows
-    , ((modm .|. shiftMask  , xK_l    ), windowSwap R False)
-    , ((modm .|. shiftMask  , xK_h    ), windowSwap L False)
-    , ((modm .|. shiftMask  , xK_j    ), windowSwap U False)
-    , ((modm .|. shiftMask  , xK_k    ), windowSwap D False)
-
-    -- Directional navigation of screens
-    , ((modm                , xK_Right), screenGo R False)
-    , ((modm                , xK_Left ), screenGo L False)
-    , ((modm                , xK_Up   ), screenGo U False)
-    , ((modm                , xK_Down ), screenGo D False)
-
-    -- Swap workspaces on adjacent screens
-    , ((modm .|. shiftMask  , xK_Right), screenSwap R False)
-    , ((modm .|. shiftMask  , xK_Left ), screenSwap L False)
-    , ((modm .|. shiftMask  , xK_Up   ), screenSwap U False)
-    , ((modm .|. shiftMask  , xK_Down ), screenSwap D False)
-
-    -- Send window to adjacent screen
-    , ((modm .|. controlMask, xK_Right), windowToScreen R False)
-    , ((modm .|. controlMask, xK_Left ), windowToScreen L False)
-    , ((modm .|. controlMask, xK_Up   ), windowToScreen U False)
-    , ((modm .|. controlMask, xK_Down ), windowToScreen D False)
-
-    -- cycle workspace
-    , ((modm                , xK_bracerigh ), nextWS                               )
-    , ((modm                , xK_braceleft ), prevWS                               )
-    , ((modm .|. shiftMask  , xK_braceright), shiftToNext                          )
-    , ((modm .|. shiftMask  , xK_braceleft ), shiftToPrev                          )
-    , ((modm .|. shiftMask  , xK_period    ), toggleWS                             )
-    , ((modm                , xK_period    ), gridselectWorkspace myGSConfWs W.view)
-    , ((modm .|. controlMask, xK_period    ), workspacePrompt def (windows . W.shift))
-
-    -- change pwd for current workspace
-    , ((modm, xK_p), changeDir myXPConf)
-
-    , ((modm                , xK_g), goToSelected  myGSConf)
-    , ((modm .|. shiftMask  , xK_g), bringSelected myGSConf)
-    , ((modm .|. controlMask, xK_g), mkXPromptWithModes [Goto, Bring, BringToMaster, BringCopy]) myXPConf -- TODO test
-
-    -- hide windows
-    , ((modm,               xK_d), withFocused hideWindow)
-    , ((modm .|. shiftMask, xK_d), popOldestHiddenWindow)
+    -- switch window
+    , ("M-.", mkXPromptWithModes [Goto, Bring, BringToMaster, BringCopy]
+                                 myXPConf) -- TODO test
 
     -- layout select
-    , ((modm .|. shiftMask, xK_semicolon), layoutPrompt myXPConf) -- TODO test
-    , ((modm, xK_semicolon), runSelectedAction myGSConfS
-        [ ("Tab"     , sendMessage $ JumpToLayout "Tab"     )
-        , ("Tiled"   , sendMessage $ JumpToLayout "Tiled"   )
-        , ("GridL"   , sendMessage $ JumpToLayout "GridL"   )
-        , ("DrapV"   , sendMessage $ JumpToLayout "DrapV"   )
-        , ("Column"  , sendMessage $ JumpToLayout "Column"  )
-        , ("Grid"    , sendMessage $ JumpToLayout "Grid"    )
-        , ("Full"    , sendMessage $ JumpToLayout "Full"    )
-        , ("DrapH"   , sendMessage $ JumpToLayout "DrapH"   )
-        , ("MTiled"  , sendMessage $ JumpToLayout "MTiled"  )
-        , ("MColumn" , sendMessage $ JumpToLayout "MColumn" )
-        ]
-      )
+    , ("M-;", layoutPrompt myXPConf) -- TODO test
 
+    -- Directional navigation of windows
+    , ("M-l", windowGo R False)
+    , ("M-h", windowGo L False)
+    , ("M-j", windowGo U False)
+    , ("M-k", windowGo D False)
+
+    -- Swap adjacent windows
+    , ("M-S-l", windowSwap R False)
+    , ("M-S-h", windowSwap L False)
+    , ("M-S-j", windowSwap U False)
+    , ("M-S-k", windowSwap D False)
+
+    -- Directional navigation of screens
+    , ("M-<Right>", screenGo R False)
+    , ("M-<Left>" , screenGo L False)
+    , ("M-<Up>"   , screenGo U False)
+    , ("M-<Down>" , screenGo D False)
+
+    -- Swap workspaces on adjacent screens
+    , ("M-S-<Right>", screenSwap R False)
+    , ("M-S-<Left>" , screenSwap L False)
+    , ("M-S-<Up>"   , screenSwap U False)
+    , ("M-S-<Down>" , screenSwap D False)
+
+    -- Send window to adjacent screen
+    , ("M-C-<Right>", windowToScreen R False)
+    , ("M-C-<Left>" , windowToScreen L False)
+    , ("M-C-<Up>"   , windowToScreen U False)
+    , ("M-C-<Down>" , windowToScreen D False)
+
+    -- cycle workspace
+    , ("M-}"  , nextWS                               )
+    , ("M-{"  , prevWS                               )
+    , ("M-S-}", shiftToNext                          )
+    , ("M-S-{", shiftToPrev                          )
+    , ("M-S-.", toggleWS                             )
+    , ("M-g"  , workspacePrompt def (windows . shift))
+
+    -- change pwd for current workspace
+    , ("M-p", changeDir myXPConf)
+
+    -- hide windows
+    , ("M-d"  , withFocused hideWindow)
+    , ("M-S-d", popOldestHiddenWindow)
     -- paste
-    , ((modm, xK_y), pasteSelection)
+    , ("M-y", pasteSelection)
 
     -- window menu
-    , ((modm, xK_z), windowMenu)
+    , ("M-w", windowMenu)
     ]
 
 myLayout = beforeLayouts layouts
@@ -220,9 +201,9 @@ myLayout = beforeLayouts layouts
                 })
         ||| renamed [ Replace "Tiled"     ] tiled
         ||| renamed [ Replace "GridL"     ] (SplitGrid L 2 3 (2/3) (16/10) (1/100))
-        ||| rename  [ Replace "DragV"     ] (dragPane Vectial -2.1 0.5 )
+        ||| renamed [ Replace "DragV"     ] (dragPane Vectial -2.1 0.5 )
         ||| renamed [ Replace "Grid"      ] (Grid (16/10))
-        ||| rename  [ Replace "DragH"     ] (dragPane Horizontal 0.1 0.5 )
+        ||| renamed [ Replace "DragH"     ] (dragPane Horizontal 0.1 0.5 )
         ||| renamed [ Replace "MTiled"    ] (Mirror tiled)
         ||| renamed [ Replace "Column"    ] column
         ||| renamed [ Replace "MColumn"   ] (Mirror column)
@@ -256,24 +237,6 @@ myDef = def
 
 -- main
 main = xmonad myDef
-
--- color define
-solarizedBase03  = "#002b36"
-solarizedBase02  = "#073642"
-solarizedBase01  = "#586e75"
-solarizedBase00  = "#657b83"
-solarizedBase0   = "#839496"
-solarizedBase1   = "#93a1a1"
-solarizedBase2   = "#eee8d5"
-solarizedBase3   = "#fdf6e3"
-solarizedYellow  = "#b58900"
-solarizedOrange  = "#cb4b16"
-solarizedRed     = "#dc322f"
-solarizedMagenta = "#d33682"
-solarizedViolet  = "#6c71c4"
-solarizedBlue    = "#268bd2"
-solarizedCyan    = "#2aa198"
-solarizedGreen   = "#859900"
 
 draculaBackground = "#282a36"
 draculaForeground = "#f8f8f2"
