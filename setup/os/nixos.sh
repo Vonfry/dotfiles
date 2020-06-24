79712c0a01788b527b378881cb53a73bce3ceab2
Vonfry <mail@vonfry.name>
2020-06-24 16:46:26 +0800

    nix/home-manager: setup fix

--- setup/os/nixos.sh
+++ setup/os/nixos.sh
@@ -15,14 +15,16 @@ sudo grep -E "^ *system\.stateVersion" /etc/nixos.bak/configuration.nix |
     sed "s/system/home/"     |
     sed "s/#.*$//"           |
     sed "s/^ */\\\\ \\\\ /g" |
-    xargs -n1 -I{} sed -i "/^ *# *home\.stateVersion$/a {}" /etc/nixos/user/vonfry/home/local/base.nix
+    xargs -n1 -I{} sed -i "/^ *# *home\.stateVersion$/a {}" /etc/nixos/user/vonfry/home/base.local.nix
 sudo grep -E "^ *boot\.loader" /etc/nixos.bak/configuration.nix |
     sed "s/#.*$//" |
     sed "s/^ */\\\\ \\\\ /g" |
     xargs -n1 -I "{}" sed -i "/^ *# boot\.loader$/a {}" /etc/nixos/base/default.local.nix
-sed -i "/^ *# *home\.user$/a \\ \\ home.username = \"$USER\";" /etc/nixos/user/vonfry/home/local/base.nix
-sed -i "/^ *# *home\.user$/a \\ \\ home.homeDirectory = \"$HOME\";" /etc/nixos/user/vonfry/home/local/base.nix
+sed -i "/^ *# *home\.user$/a \\ \\ home.username = \"$USER\";" /etc/nixos/user/vonfry/home/base.local.nix
+sed -i "/^ *# *home\.user$/a \\ \\ home.homeDirectory = \"$HOME\";" /etc/nixos/user/vonfry/home/base.local.nix
 rm -rf /etc/nixos.bak
+echo_info
+"--- please set some local files for home-manager or configuration under $script_dir/etc/nixos/local and $script_dir/etc/nixos/user/vonfry/home/local"
 sudo nix-channel --add http://nixos.org/channels/nixos-unstable nixos-unstable
 sudo nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 
