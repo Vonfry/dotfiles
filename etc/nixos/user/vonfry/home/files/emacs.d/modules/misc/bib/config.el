29471c64bd01a99cfd1386ba47c6f6db76d3be76
Vonfry <mail@vonfry.name>
2020-07-03 21:14:34 +0800

    emacs: ebib list order

--- etc/nixos/user/vonfry/home/files/emacs.d/modules/misc/bib/config.el
+++ etc/nixos/user/vonfry/home/files/emacs.d/modules/misc/bib/config.el
@@ -1,19 +1,28 @@
 ;;; bib config -*- lexical-binding: t -*-
 ;;
+(const! +bib--set-counter most-negative-fixnum)
 
 (fun! +bib--set-dir (name val)
   "set bib directory."
   (set-default name val)
   (when val
+    (when (= +bib--set-counter most-negative-fixnum)
+      (setq +bib-search-dir nil
+            ebib-file-search-dirs nil))
+    (setq +bib--set-counter (1- +bib--set-counter))
     (eval `(custom-set!
             ,@(if +bib-note-is-single
                 (list 'ebib-notes-file (expand-file-name "notes.org" val))
                 (list 'ebib-notes-directory val))
             ebib-reading-list-file (expand-file-name "reading.org" ,val)
             +bib-search-dir (add-to-ordered-list
-                              '+bib-search-dir ,val 0)
+                             '+bib-search-dir
+                             ,val
+                             +bib--set-counter)
             ebib-file-search-dirs (add-to-ordered-list
-                                    'ebib-file-search-dirs ,val 0)))))
+                                   'ebib-file-search-dirs
+                                   ,val
+                                   +bib--set-counter)))))
 
 (custom! +bib-note-is-single t
   "bib notes use a single file or not. see `ebib-notes-directory' and
