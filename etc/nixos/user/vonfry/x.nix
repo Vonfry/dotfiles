c50a6e7eff315a1ffb72074ab6205eda80d81589
Vonfry <mail@vonfry.name>
2020-09-06 09:54:21 +0800

    emacs: org-protocol

--- etc/nixos/user/vonfry/x.nix
+++ etc/nixos/user/vonfry/x.nix
@@ -114,5 +114,16 @@
     enable = true;
     userDirs.enable = true;
     mimeApps.enable = true;
+    dataFiles = {
+      "applications/org-protocol.desktop".text = ''
+        [Desktop Entry]
+        Name=org-protocol
+        Exec=emacsclient %u
+        Type=Application
+        Terminal=false
+        Categories=System;
+        MimeType=x-scheme-handler/org-protocol;
+      '';
+    };
   };
 }
