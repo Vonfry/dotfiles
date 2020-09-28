8ed2a4f4b9121fa49c34ca0ad788214d81542108
Vonfry <mail@vonfry.name>
2020-09-28 09:41:31 +0800

    zathura: remove increment search

--- etc/nixos/user/vonfry/application.nix
+++ etc/nixos/user/vonfry/application.nix
@@ -9,7 +9,6 @@
         map t recolor
       '';
       options = {
-        incremental-search      = true;
         font                    = "monospace normal 11";
         notification-error-bg   = "#ff5555"; # Red
         notification-error-fg   = "#f8f8f2"; # Foreground
@@ -82,7 +81,7 @@
       pavucontrol audacious # kid3 audacity
       # mpv ffmpeg
       unstable.tor-browser-bundle-bin
-      zathura
+      # zathura
       libreoffice
     ];
   };
