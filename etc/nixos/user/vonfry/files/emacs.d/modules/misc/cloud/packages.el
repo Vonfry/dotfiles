20b33f74de22b15158a5344594a5aa0ac383362f
Vonfry <mail@vonfry.name>
2020-10-05 09:31:07 +0800

    emacs: change keybind

--- etc/nixos/user/vonfry/files/emacs.d/modules/misc/cloud/packages.el
+++ etc/nixos/user/vonfry/files/emacs.d/modules/misc/cloud/packages.el
@@ -5,8 +5,8 @@
 ;; rclone mount is good, but slowly to edit file, each time we need to wait for
 ;; fetching date, so I use this method to sync date manual.
 (+mmap-at-def
-  "C"   '(nil :which-key "cloud sync")
-  "C g" '+cloud-get
-  "C p" '+cloud-push
-  "C c" '+cloud-copy-to
-  "C C" '+cloud-copy-from)
+  "s"   '(nil :which-key "cloud sync")
+  "s g" '+cloud-get
+  "s p" '+cloud-push
+  "s c" '+cloud-copy-to
+  "s C" '+cloud-copy-from)
