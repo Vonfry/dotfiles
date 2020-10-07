af2adab2d4a13c16af6e9e87f3df863d9680cdaf
Vonfry <mail@vonfry.name>
2020-10-07 15:07:46 +0800

    home-manager: add zathura to default mime type

--- etc/nixos/user/vonfry/application.nix
+++ etc/nixos/user/vonfry/application.nix
@@ -104,6 +104,7 @@
       "application/xhtml+xml"         = "org.qutebrowser.qutebrowser.desktop";
       "application/x-extension-xhtml" = "org.qutebrowser.qutebrowser.desktop";
       "application/x-extension-xht"   = "org.qutebrowser.qutebrowser.desktop";
+      "application/pdf"               = "org.pwmt.zathura.desktop";
     };
   };
 
