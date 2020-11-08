739ac91150311c7a25ed2f5e2e25f262707d70f8
Vonfry <mail@vonfry.name>
2020-11-08 10:58:34 +0800

    tmux: fixup

--- etc/nixos/modules/user/shell.nix
+++ etc/nixos/modules/user/shell.nix
@@ -10,11 +10,9 @@ in {
         enable = true;
         clock24 = true;
         keyMode = "vi";
-        shortcut = "\\";
         extraConfig = ''
           set -g update-environment -r
           set -g status-right "#{tmux_mode_indicator}"
-          unbind C-b
         '';
       };
 
