96818675188435fb4096d3e4d5079f04dd9daab9
Vonfry <mail@vonfry.name>
2020-11-17 19:19:35 +0800

    git: auto squash by default

--- etc/nixos/modules/user/development.nix
+++ etc/nixos/modules/user/development.nix
@@ -273,6 +273,7 @@ in {
         enable = true;
         extraConfig = {
           pull.rebase = mkDefault true;
+          rebase.autoSquash = mkDefault true;
           github.user = "Vonfry";
           gitlab.user = "Vonfry";
         };
