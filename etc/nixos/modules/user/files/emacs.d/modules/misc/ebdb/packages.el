0c8ee9caec40335dec9c58ed7863e02fe7ad6d3a
Vonfry <mail@vonfry.name>
2020-12-08 10:40:10 +0800

    emacs/ebdb: evil state

--- etc/nixos/modules/user/files/emacs.d/modules/misc/ebdb/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/misc/ebdb/packages.el
@@ -5,6 +5,9 @@
   :custom
   (ebdb-sources (directory-files (expand-file-name "db" +ebdb-base-dir) t "^[A-z0-9_\\-]+$"))
   (ebdb-image-path (expand-file-name "imgs" +ebdb-base-dir))
+  :hook
+  (evil-mode . (lambda ()
+    (evil-set-initial-state 'ebdb-mode  'emacs)))
   :general
   (+mmap-at-def
     "c"   'ebdb-counsel
