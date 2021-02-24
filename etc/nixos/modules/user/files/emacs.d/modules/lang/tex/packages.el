29447f44441f4e35713861c09222b2f303d49ab5
Vonfry <mail@vonfry.name>
2021-02-24 12:45:37 +0800

    emacs/tex: tex region path

--- etc/nixos/modules/user/files/emacs.d/modules/lang/tex/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/tex/packages.el
@@ -20,7 +20,7 @@
   (TeX-auto-local "latex.out")
   (TeX-style-local TeX-auto-local)
   (TeX-auto-private (expand-file-name "tex/" vonfry-cache-dir))
-  (TeX-region (expand-file-name "_region_" TeX-auto-local))
+  (TeX-region (concat TeX-auto-local "/_region_"))
   (TeX-view-program-selection '((output-pdf "zathura")
                                 (output-dvi "xdvi")
                                 (output-html "xdg-open")))
