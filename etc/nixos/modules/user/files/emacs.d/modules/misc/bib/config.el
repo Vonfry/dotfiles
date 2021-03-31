df87978fd7b7c1319aa5a6e6594c4306defd36db
Vonfry <mail@vonfry.name>
2021-03-31 10:13:01 +0800

    emacs/bib: use single note file

--- etc/nixos/modules/user/files/emacs.d/modules/misc/bib/config.el
+++ etc/nixos/modules/user/files/emacs.d/modules/misc/bib/config.el
@@ -1,7 +1,7 @@
 ;;; bib config -*- lexical-binding: t -*-
 ;;
 
-(defcustom +bib-note-is-single nil
+(defcustom +bib-note-is-single t
   "bib notes use a single file or not. see `ebib-notes-directory' and
 `ebib-notes-file'"
   :type 'boolean
