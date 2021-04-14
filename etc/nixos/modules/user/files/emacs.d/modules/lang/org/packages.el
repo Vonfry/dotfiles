05c339f2e7b8a0a3ebde335ba4cfedf9e3db63db
Vonfry <mail@vonfry.name>
2021-04-14 10:43:46 +0800

    emacs/org: archive tag inherit

--- etc/nixos/modules/user/files/emacs.d/modules/lang/org/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/org/packages.el
@@ -50,6 +50,7 @@
       "** %?\n:PROPERTIES:\n:CREATED: %U\n:END:")
      ("c" "Contacts" entry (file+headline +org-capture-file "Contacts")
       "** %(org-contacts-template-name)\n:PROPERTIES:\n:EMAIL: %(org-contacts-template-email)\s\n:PHONE:\n:ALIAS:\n:NICKNAME:\n:IGNORE:\n:ICON:\n:NOTE:\n:ADDRESS:\n:BIRTHDAY:\n:END:")))
+  (org-archive-subtree-add-inherited-tags t)
   :general
   ("C-c C" 'org-capture-goto-target)
   ("C-c a" 'org-agenda)
