97391700c2bf9880cde39710d3fdc07470766e4f
Vonfry <mail@vonfry.name>
2021-06-02 00:03:09 +0800

    fcitx5: add tables

--- etc/nixos/modules/misc.nix
+++ etc/nixos/modules/misc.nix
@@ -7,7 +7,8 @@ in {
   config = mkIf cfg.enable {
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-table-other fcitx5-rime fcitx5-mozc
+      fcitx5.addons = with pkgs; [ fcitx5-table-extra fcitx5-rime fcitx5-mozc
+                                   fcitx5-chinese-addons
                                  ];
     };
 
