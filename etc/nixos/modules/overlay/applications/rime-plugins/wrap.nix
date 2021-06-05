f2df99b3b2ea36811438bdc83c453d8baf6fcd8c
Vonfry <mail@vonfry.name>
2021-06-05 22:59:13 +0800

    rime: refile and add more config

--- etc/nixos/modules/overlay/applications/rime-plugins/wrap.nix
+++
@@ -1,8 +0,0 @@
-{ runCommand }:
-
-src:
-
-runCommand "rime-${src.name}" {} ''
-  mkdir -p $out/share/rime-plugins
-  ln -s ${src}/*.yaml $out/share/rime-plugins
-''
--- etc/nixos/modules/user/x.nix
+++ etc/nixos/modules/user/x.nix
@@ -130,23 +130,18 @@ in {
           MimeType=x-scheme-handler/org-protocol;
         '';
 
-        "fcitx5/rime/default.custom.yaml" = {
-          source = ./files/rime/default.custom.yaml;
-          onChange = ''
-          [ -f ${toString config.xdg.dataHome}/fcitx5/rime/default.yaml ] && rm ${toString config.xdg.dataHome}/fcitx5/rime/default.yaml
-          fcitx5-remote -r
-        '';
-        };
         "fcitx5/rime" = {
           source = with pkgs; with vonfryPackages.rimePlugins;
             runCommand "fcitx-rime-plugins" {} ''
-            mkdir $out
-            ln -s ${cangjie}/share/rime-plugins/*.yaml $out
-            ln -s ${wubi86-jidian}/share/rime-plugins/wubi86*.yaml $out
-            ln -s ${wubi86-jidian}/share/rime-plugins/pinyin_simp.*.yaml $out
-            ln -s ${wubi86-jidian}/share/rime-plugins/numbers.*.yaml $out
-            ln -s ${japanese}/share/rime-plugins/*.yaml $out
-          '';
+              mkdir -p $out
+              cp ${./files/rime/default.custom.yaml} $out/default.custom.yaml
+              cp ${cangjie}/share/rime/cangjie5.*.yaml $out
+              cp ${wubi86-jidian}/share/rime/numbers.*.yaml $out
+              cp ${wubi86-jidian}/share/rime/wubi86_jidian.*.yaml $out
+              cp ${wubi86-jidian}/share/rime/wubi86_jidian_extra.*.yaml $out
+              cp ${wubi86-jidian}/share/rime/wubi86_jidian_trad.*.yaml $out
+              cp ${japanese}/share/rime/japanese.*.yaml $out
+            '';
           recursive = true;
         };
         "fcitx5/themes/Material-Color/theme.conf".source = "${pkgs.vonfryPackages.fcitx5-theme.material-color}/theme-deepPurple.conf";
