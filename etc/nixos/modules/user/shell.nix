2c588023f71196a3f9a217c6bcdb8f08b768a204
Vonfry <mail@vonfry.name>
2021-06-08 14:30:14 +0800

    zsh: set windows title

--- etc/nixos/modules/user/shell.nix
+++ etc/nixos/modules/user/shell.nix
@@ -119,6 +119,11 @@ in {
           done
 
           unset keymap
+
+          function set_win_title() {
+            echo -ne "\033]0; $(basename "$PWD") \007"
+          }
+          precmd_functions+=(set_win_title)
         '';
 
         loginExtra = ''
