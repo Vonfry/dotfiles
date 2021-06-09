a9d8e65c5bb1aa661932f4e50245844858e1f787
Vonfry <mail@vonfry.name>
2021-06-09 09:26:58 +0800

    sddm: config face into global path instead of user one, this can avoid acl

--- etc/nixos/modules/user/shell.nix
+++ etc/nixos/modules/user/shell.nix
@@ -500,11 +500,6 @@ in {
 
             ! [ -f ${toString configHome}/bg.png ] && ln -s ${pkgs.vonfryPackages.desktopBackground} ${toString configHome}/bg.png
 
-            if ! [ -f ~/.face.icon ]; then
-              cp ${pkgs.vonfryPackages.icon-face} .face.icon
-              setfacl -m u:sddm:x ~/
-              setfacl -m u:sddm:r ~/.face.icon
-            fi
           '';
         in lib.hm.dag.entryAfter ["writeBoundary"]
           (concatStringsSep "\n" [ linkNormal linkOrg linkEmacs makeLib ]);
