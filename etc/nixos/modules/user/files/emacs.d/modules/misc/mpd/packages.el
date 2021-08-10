d9410c6b3d001a1b3994f3a1f7847c2594571cd1
Vonfry <mail@vonfry.name>
2021-08-10 16:52:20 +0800

    Revert "emacs/mpd: use core-map as keybind"
    This reverts commit d9bbc777173e9d41709e2ebd593d7972f653f2b0.

--- etc/nixos/modules/user/files/emacs.d/modules/misc/mpd/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/misc/mpd/packages.el
@@ -3,4 +3,33 @@
 
 (use-package mpdel
   :general
-  (nmap-at "m" mpdel-core-map))
+  (nmap-at
+    "m"     '(:ignore t :which-key "mpd")
+    "m m"   'mpdel-mode
+    "m SPC" 'libmpdel-playback-play-pause
+    "m n"   'libmpdel-playback-next
+    "m p"   'libmpdel-playback-previous
+    "m a"   'mpdel-core-add-to-current-playlist
+    "m A"   'mpdel-core-add-to-stored-playlist
+    "m r"   'mpdel-core-replace-current-playlist
+    "m R"   'mpdel-core-replace-stored-playlist
+    "m P"   'mpdel-core-insert-current-playlist
+    "m D"   'mpdel-core-dired
+    "m N"   'mpdel-core-open-artists
+    "m L"   'mpdel-core-open-stored-playlists
+    "m s s" 'mpdel-core-search-by-title
+    "m s l" 'mpdel-core-search-by-album
+    "m s r" 'mpdel-core-search-by-artist
+    "m ^"   'navigel-open-parent
+    "m +"   'mpdel-core-volume-increase
+    "m -"   'mpdel-core-volume-decrease
+    "m C"   'libmpdel-connect-profile
+    "m l"   'mpdel-playlist-open
+    "m v"   'mpdel-song-open
+    "m f"   'mpdel-song-normal-increment
+    "m F"   'mpdel-song-small-increment
+    "m o"   'mpdel-browser-open
+    "m M-f" 'mpdel-song-large-increment
+    "m b"   'mpdel-song-normal-decrement
+    "m B"   'mpdel-song-small-decrement
+    "m M-b" 'mpdel-song-large-decrement))
