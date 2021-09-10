ae8ec068fe98c33cd0d3ee63599504ce739b86aa
Vonfry <mail@vonfry.name>
2021-09-10 13:21:29 +0800

    emacs/yasnippet: create yas nippet local dir at first

--- etc/nixos/modules/user/files/emacs.d/modules/completion/yasnippet/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/completion/yasnippet/packages.el
@@ -2,9 +2,12 @@
 ;;
 
 (use-package yasnippet
+  :init
+  (unless (file-exists-p +yasnippet-local-dir)
+    (make-directory +yasnippet-local-dir))
   :custom
   (yas-snippet-dirs
-   (list (expand-file-name "snippets/" vonfry-local-dir)
+   (list '+yasnippet-local-dir
          (expand-file-name "completion/yasnippet/snippets" vonfry-modules-dir)
          'yasnippet-snippets-dir))
   :general
