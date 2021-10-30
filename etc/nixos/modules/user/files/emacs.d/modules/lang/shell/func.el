c8d9c758a0019ce08b67ac8e389a10df50926037
Vonfry <mail@vonfry.name>
2021-10-30 21:15:33 +0800

    emacs/shell: add help function for ansi color

--- etc/nixos/modules/user/files/emacs.d/modules/lang/shell/func.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/shell/func.el
@@ -5,3 +5,12 @@
   "Open file manager in system. This function is worked with `org-open-file'."
   (interactive)
   (org-open-file "." 'system))
+
+(defun +shell/display-ansi-colors ()
+  (interactive)
+  (ansi-color-apply-on-region (point-min) (point-max)))
+
+(defun +shell/display-ansi-colors ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
