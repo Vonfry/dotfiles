b43ec64ecfc75ed239808837332998a85407fd1b
Vonfry <mail@vonfry.name>
2021-11-19 10:01:53 +0800

    emacs/agda: remove duplication defination

--- etc/nixos/modules/user/files/emacs.d/modules/lang/agda/func.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/agda/func.el
@@ -1,13 +1,3 @@
 ;;; agda func -*- lexical-binding: t -*-
 ;;
 
-(defun +agda/setup ()
-  "setup agda mode path due to the installation with nix in project instead of
-globally."
-  (interactive)
-  (let ((agda-mode-exec (executable-find "agda-mode")))
-    (when agda-mode-exec
-      (load
-        (with-temp-buffer
-          (call-process agda-mode-exec nil t nil "locate")
-          (buffer-string))))))
