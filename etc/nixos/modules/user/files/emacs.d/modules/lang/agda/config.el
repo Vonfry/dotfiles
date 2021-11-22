3a22ac17945d29ce867adc4b2dd704885828c2f6
Vonfry <mail@vonfry.name>
2021-11-22 08:15:35 +0800

    emacs/agda: set exec-path manually
    Because hacking variables before major mode is hard, I have to open a new emacs
    for agda working.

--- etc/nixos/modules/user/files/emacs.d/modules/lang/agda/config.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/agda/config.el
@@ -5,12 +5,13 @@
   "setup agda mode path due to the installation with nix in project instead of
 globally."
   (interactive)
-  (let ((agda-mode-exec (executable-find "agda-mode")))
+  (let* ((agda-mode-exec (executable-find "agda-mode"))
+         (agda-exec (directory-file-name (file-name-directory agda-mode-exec))))
     (when agda-mode-exec
+      (add-to-list 'exec-path agda-exec)
       (load
         (with-output-to-string
-          (call-process agda-mode-exec nil standard-output nil "locate")))
-      (directory-file-name (file-name-directory agda-mode-exec)))))
+          (call-process agda-mode-exec nil standard-output nil "locate"))))))
 
 (defun +agda/unload ()
   "unload agda mode to let us setup next agda version."
