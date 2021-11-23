5c0db007050be74cf34b73238147f3d4583554f2
Vonfry <mail@vonfry.name>
2021-11-23 09:38:36 +0800

    nixos: fcitx5 only with rime because fcitx5-rime is fixed in new version

--- etc/nixos/modules/x.nix
+++ etc/nixos/modules/x.nix
@@ -93,8 +93,7 @@ in {
 
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-chinese-addons
-                                 ];
+      fcitx5.addons = with pkgs; [ fcitx5-rime ];
     };
 
     fonts = {
