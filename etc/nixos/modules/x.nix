d1f263e5e14aebf9d8ccaba762d4d7faf46a9861
Vonfry <mail@vonfry.name>
2021-11-27 16:28:36 +0800

    todo & xmonad: add overlay for 0_17_0

--- etc/nixos/modules/x.nix
+++ etc/nixos/modules/x.nix
@@ -66,6 +66,18 @@ in {
       };
     };
 
+
+    nixpkgs = {
+      overlays = [(s: p:  {
+        haskellPackages = super.haskellPackages.override {
+          overrides = hself: hsuper: {
+            xmonad = self.xmonad_0_17_0;
+            xmonad-contrib = self.xmonad-contrib_0_17_0;
+          };
+        };
+      })];
+    };
+
     programs = {
       dconf.enable = true;
       xss-lock = {
