e2354d0d297aee254ea171966e7e884728dec468
Vonfry <mail@vonfry.name>
2021-12-01 14:32:19 +0800

    home-manager: add sync config script due to flake restricted mode

--- etc/nixos/modules/user/misc.nix
+++ etc/nixos/modules/user/misc.nix
@@ -3,8 +3,21 @@
 with lib;
 let
   cfg = config.vonfry;
+
+  sVar = config.home.sessionVariables;
+  hasCloud = sVar ? "CLOUD_DIR";
+  inherit (sVar) DOTFILES_DIR CLOUD_DIR;
+
+  syncConfig = with pkgs; writeScriptBin "nixos-sync-config" ''
+    #!/usr/bin/env bash
+    ${sudo} ${rsync} -auP ${DOTFILES_DIR}/etc/nixos/modules /etc/nixos/modules
+      ${optionalString hasCloud ''
+        ${sudo} ${rsync} -auP ${CLOUD_DIR}/etc/nixos/local /etc/nixos/local
+        ${sudo} ${rsync} -auP ${CLOUD_DIR}/config/nixpkgs/home/local /etc/nixos/local
+      ''}
+  '';
 in {
   config = mkIf cfg.enable {
-
+    home.packages = [ syncConfig ];
   };
 }
