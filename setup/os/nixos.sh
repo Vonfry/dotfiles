2821c56ddf1d5e09304da25538c5d032a46459dd
Vonfry <mail@vonfry.name>
2021-12-01 15:19:01 +0800

    nixos: use hard link to sync

--- setup/os/nixos.sh
+++ setup/os/nixos.sh
@@ -2,7 +2,7 @@ echo_info "* system packages: Nix"
 sudo nixos-generate-config
 sudo tar -cf /etc/nixos.bak.tar /etc/nixos
 sudo mkdir -p /etc/nixos/local
-sudo rsync -auP $script_dir/etc/nixos/modules /etc/nixos/modules
+sudo cp -rl $script_dir/etc/nixos/modules/* /etc/nixos/modules
 sudo cp $script_dir/etc/nixos/configuration.nix.example /etc/nixos/configuration.nix
 sudo cp $script_dir/etc/nixos/flake.* /etc/nixos/
 echo_info "--- set some local configuration"
