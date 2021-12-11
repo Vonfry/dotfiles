c3088b176e07e01d370320256961ce19c4ce3148
Vonfry <mail@vonfry.name>
2021-12-11 15:21:32 +0800

    nixos: flake update

--- etc/nixos/modules/user/misc.nix
+++ etc/nixos/modules/user/misc.nix
@@ -8,7 +8,29 @@ let
   hasCloud = sVar ? "CLOUD_DIR";
   inherit (sVar) DOTFILES_DIR CLOUD_DIR;
 
-  syncConfig = with pkgs; writeScriptBin "nixos-sync-config" ''
+  syncModules = with pkgs; writeScriptBin "nixos-sync-modules" ''
+    #!/usr/bin/env bash
+    sudo rsync -auP ${DOTFILES_DIR}/etc/nixos/modules/ /etc/nixos/modules/
+  '';
+
+  syncLocal = with pkgs; writeScriptBin "nixos-sync-local" ''
+    #!/usr/bin/env bash
+    ${optionalString hasCloud ''
+      sudo rsync -auP ${CLOUD_DIR}/dotfiles/etc/nixos/local/ /etc/nixos/local/
+      sudo rsync -auP ${CLOUD_DIR}/dotfiles/config/nixpkgs/home/local/ /etc/nixos/local/
+    ''}
+  '';
+
+  syncFlake = with pkgs; writeScriptBin "nixos-sync-flake" ''
+    #!/usr/bin/env bash
+    sudo rsync -auP ${DOTFILES_DIR}/etc/nixos/modules/ /etc/nixos/modules/
+      ${optionalString hasCloud ''
+        sudo rsync -auP ${CLOUD_DIR}/dotfiles/etc/nixos/local/ /etc/nixos/local/
+        sudo rsync -auP ${CLOUD_DIR}/dotfiles/config/nixpkgs/home/local/ /etc/nixos/local/
+      ''}
+  '';
+
+  syncNixOS = with pkgs; writeScriptBin "nixos-sync-flake" ''
     #!/usr/bin/env bash
     sudo rsync -auP ${DOTFILES_DIR}/etc/nixos/modules/ /etc/nixos/modules/
       ${optionalString hasCloud ''
