d74ce07a4547c211298c2c40e929fb091345c82c
Vonfry <mail@vonfry.name>
2022-05-16 21:40:38 +0800

    emacs/dirvish: remove old options

--- etc/nixos/modules/user/files/emacs.d/modules/files/dirvish/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/files/dirvish/packages.el
@@ -3,7 +3,6 @@
 (use-package dirvish
   :custom
   (dirvish-cache-dir (expand-file-name "dirvish" vonfry-cache-dir))
-  (dirvish-header-style nil)
   :config
   (dirvish-override-dired-mode 1)
   :general
