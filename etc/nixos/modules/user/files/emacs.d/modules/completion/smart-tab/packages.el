e1b03fea5ef84d342ffc2a2f0e049c83fd15806e
Vonfry <mail@vonfry.name>
2022-05-21 15:44:43 +0800

    emacs/smart-tab: open hippie

--- etc/nixos/modules/user/files/emacs.d/modules/completion/smart-tab/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/completion/smart-tab/packages.el
@@ -3,6 +3,7 @@
 
 (use-package smart-tab
   :custom
+  (smart-tab-using-hippie-expand t)
   (smart-tab-user-provided-completion-function 'completion-at-point)
   (smart-tab-disabled-major-modes '(org-mode term-mode eshell-mode eww-mode
                                     magit-mode telega-chat-mode))
