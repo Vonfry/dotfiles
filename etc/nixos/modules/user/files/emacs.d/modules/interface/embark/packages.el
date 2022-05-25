0d49474cc7ba54d566c3052b9dfdeb5aa1e04937
Vonfry <mail@vonfry.name>
2022-05-25 09:08:39 +0800

    emacs/embark: change keybinding to avoid overriding

--- etc/nixos/modules/user/files/emacs.d/modules/interface/embark/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/interface/embark/packages.el
@@ -5,8 +5,8 @@
   :custom
   (prefix-help-command 'embark-prefix-help-command)
   :general
-  ("C-." 'embark-act
-   "C-," 'embark-dwim))
+  ("C-&" 'embark-act
+   "C-%" 'embark-dwim))
 
 (use-package embark-consult
   :after (embark consult)
