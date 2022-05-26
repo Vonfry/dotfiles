51487ac6be13e21c67accb513619947d93d4acda
Vonfry <mail@vonfry.name>
2022-05-26 08:38:07 +0800

    nixos/flake: pin to 22.05

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
