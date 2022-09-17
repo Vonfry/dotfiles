780898278d9d2285bc10fec4254c0a0518a83999
Vonfry <mail@vonfry.name>
2022-09-17 09:17:05 +0800

    emacs/agda & pg: modify some keybindings

--- etc/nixos/modules/user/files/emacs.d/modules/lang/agda/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/agda/packages.el
@@ -7,40 +7,44 @@
   (+agda--mode-load)
   :general
   (nmap-leader :keymaps 'agda2-mode-map
-    "("   'agda2-infer-type-maybe-toplevel
-    "&"   'agda2-search-about-toplevel
-    ";"   'agda2-module-contents-maybe-toplevel
-    ","   'agda2-go-back
     "."   'agda2-goto-definition-keyboard
+    ","   'agda2-go-back
+    ";"   'agda2-module-contents-maybe-toplevel
+    "&"   'agda2-search-about-toplevel
     "r"   'agda2-compile
-    "{"   'agda2-auto-maybe-all
-    "} ." 'agda2-goal-and-context-and-inferred
-    "} ;" 'agda2-goal-and-context-and-checked
-    "} t" 'agda2-show-context
-    "} r" 'agda2-solve-maybe-all
-    "} {" 'agda2-show-goals
-    "} n" 'agda2-next-goal
-    "} p" 'agda2-previous-goal
-    "} t" 'agda2-goal-type
-    "} r" 'agda2-refine
-    "} c" 'agda2-make-case
-    "?"   'agda2-helper-function-type
-    "t"   'agda2-goal-and-context
-    "["   'agda2-why-in-scope-maybe-toplevel
-    "("   'agda2-compute-normalised-maybe-toplevel
-    ")"   'agda2-show-constraints
+    ")"   'eri-indent
+    "("   'eri-indent-reverse
     "c x" 'agda2-comment-dwim-rest-of-buffer
     "' b" 'agda2-load
+    "r"   'agda2-compile
 
     "RET"   'agda2-give
     "' RET" 'agda2-elaborate-give)
   (nmap-mode :keymaps 'agda2-mode-map
-    "q" 'agda2-quit
-    "R" 'agda2-restart
-    "A" 'agda2-abort
-    "d" 'agda2-remove-annotations
-    "h" 'agda2-display-implicit-arguments
-    "i" 'agda2-display-irrelevant-arguments
-    "S" 'agda2-set-program-version
-    ")" 'eri-indent
-    "(" 'eri-indent-reverse))
+    "q"   'agda2-quit
+    "R"   'agda2-restart
+    "A"   'agda2-abort
+    "d"   'agda2-remove-annotations
+    "h"   'agda2-display-implicit-arguments
+    "i"   'agda2-display-irrelevant-arguments
+    "S"   'agda2-set-program-version
+    "a"   'agda2-auto-maybe-all
+    "d"   'agda2-infer-type-maybe-toplevel
+    ","   'agda2-goal-and-context
+    "."   'agda2-goal-and-context-and-inferred
+    ";"   'agda2-goal-and-context-and-checked
+    "e"   'agda2-show-context
+    "s"   'agda2-solve-maybe-all
+    "?"   'agda2-show-goals
+    "n"   'agda2-next-goal
+    "p"   'agda2-previous-goal
+    "t"   'agda2-goal-type
+    "r"   'agda2-refine
+    "c"   'agda2-make-case
+    "N"   'agda2-compute-normalised-maybe-toplevel
+    ")"   'agda2-show-constraints
+    "w"   'agda2-why-in-scope-maybe-toplevel
+    "v"   'agda2-display-program-version
+    "V"   'agda2-set-program-version
+    "?"   'agda2-helper-function-type
+    ))
