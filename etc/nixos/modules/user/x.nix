c16f6a2450a3bd77e65fb116573cebb164475fa5
Vonfry <mail@vonfry.name>
2022-11-27 11:11:16 +0800

    home-manager/rime: link default yaml because it is removed from our pack

--- etc/nixos/modules/user/x.nix
+++ etc/nixos/modules/user/x.nix
@@ -137,6 +137,7 @@ in {
               mkdir -p $out
               cp ${./files/rime/default.custom.yaml} $out/default.custom.yaml
               cp ${./files/rime/wubi86_jidian.custom.yaml} $out/wubi86_jidian.custom.yaml
+              cp ${prelude}/share/rime/default.yaml $out
               cp ${prelude}/share/rime/symbols.yaml $out
               cp ${prelude}/share/rime/punctuation.yaml $out
               cp ${cangjie}/share/rime/cangjie5.*.yaml $out
