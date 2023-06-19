9910f6e2cd31257564a85064e9ecf9b8d6b3d543
Vonfry <mail@vonfry.name>
2023-06-19 19:01:10 +0800

    nix: update flake and apply new emacs option

--- etc/nixos/modules/user/development.nix
+++ etc/nixos/modules/user/development.nix
@@ -131,7 +131,7 @@ in {
       };
 
       emacs =  {
-        package = pkgs.emacs-git.override { nativeComp = false; }; # wait for fix
+        package = pkgs.emacs-git.override { withNativeCompilation = false; }; # wait for fix
         enable = true;
         extraPackages = epkgs: with epkgs; [
           solarized-theme
