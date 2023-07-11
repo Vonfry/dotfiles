35bde2e2ec260e556580a9b0ee06e7be6f75bf3f
Vonfry <mail@vonfry.name>
2023-07-11 20:48:19 +0800

    emacs: remove all-the-icons

--- etc/nixos/modules/user/files/emacs.d/core/vonfry-ui.el
+++ etc/nixos/modules/user/files/emacs.d/core/vonfry-ui.el
@@ -3,8 +3,6 @@
 ;; Set emacs ui with the default variables and other theme.
 ;; The default theme is solarized.
 
-(use-package all-the-icons) ;; M-x all-the-icons-install-fonts and the run (fc-cache -f -v) with shell
-
 (use-package solarized
   :ensure solarized-theme
   :custom
