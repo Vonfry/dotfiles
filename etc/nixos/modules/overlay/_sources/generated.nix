52e779914ee29f2404cded2c88273ff5fef56b91
Vonfry <mail@vonfry.name>
2023-08-26 20:27:05 +0800

    nix/overlay: update

--- etc/nixos/modules/overlay/_sources/generated.nix
+++ etc/nixos/modules/overlay/_sources/generated.nix
@@ -11,7 +11,7 @@
   };
   background-image = {
     pname = "background-image";
-    version = "2023-08-03";
+    version = "2023-08-26";
     src = fetchurl {
       url = "https://wiki.haskell.org/wikistatic/haskellwiki_logo.png";
       sha256 = "sha256-NVUPqd/3lxzRPrTfBCBXgxrXR3blEJC/XiJ1nAEcaPQ=";
@@ -20,112 +20,112 @@
   fcitx5-material-color = {
     pname = "fcitx5-material-color";
     version = "2256feeae48dcc87f19a3cfe98f171862f8fcace";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "hosxy";
       repo = "fcitx5-material-color";
       rev = "2256feeae48dcc87f19a3cfe98f171862f8fcace";
       fetchSubmodules = false;
       sha256 = "sha256-i9JHIJ+cHLTBZUNzj9Ujl3LIdkCllTWpO1Ta4OT1LTc=";
-    });
+    };
     date = "2021-02-21";
   };
   fcitx5-nord = {
     pname = "fcitx5-nord";
     version = "bdaa8fb723b8d0b22f237c9a60195c5f9c9d74d1";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "tonyfettes";
       repo = "fcitx5-nord";
       rev = "bdaa8fb723b8d0b22f237c9a60195c5f9c9d74d1";
       fetchSubmodules = false;
       sha256 = "sha256-qVo/0ivZ5gfUP17G29CAW0MrRFUO0KN1ADl1I/rvchE=";
-    });
+    };
     date = "2021-07-27";
   };
   fortune-chinese = {
     pname = "fortune-chinese";
     version = "000fb01261e4d119e4e988ce82f49fb8b139fe3c";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "ruanyf";
       repo = "fortunes";
       rev = "000fb01261e4d119e4e988ce82f49fb8b139fe3c";
       fetchSubmodules = false;
       sha256 = "sha256-O258vnAHQ3RrJnMPmVntmkj+RSfpHsf/YKJcLZd0owc=";
-    });
+    };
     date = "2021-08-13";
   };
   rime-cangjie = {
     pname = "rime-cangjie";
     version = "8dfad9e537f18821b71ba28773315d9c670ae245";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "rime";
       repo = "rime-cangjie";
       rev = "8dfad9e537f18821b71ba28773315d9c670ae245";
       fetchSubmodules = false;
       sha256 = "sha256-fmWGgYqWndCpDUV6nzx0zjkcf5AcVeDIYwp0023iMwk=";
-    });
+    };
     date = "2021-02-23";
   };
   rime-japanese = {
     pname = "rime-japanese";
     version = "4c1e65135459175136f380e90ba52acb40fdfb2d";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "gkovacs";
       repo = "rime-japanese";
       rev = "4c1e65135459175136f380e90ba52acb40fdfb2d";
       fetchSubmodules = false;
       sha256 = "sha256-/mIIyCu8V95ArKo/vIS3qAiD8InUmk8fAF/wejxRxGw=";
-    });
+    };
     date = "2023-08-02";
   };
   rime-prelude = {
     pname = "rime-prelude";
     version = "dd84abecc33f0b05469f1d744e32d2b60b3529e3";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "rime";
       repo = "rime-prelude";
       rev = "dd84abecc33f0b05469f1d744e32d2b60b3529e3";
       fetchSubmodules = false;
       sha256 = "sha256-r3jx/iCUOxBFLYhmHEuSFxzmHg8l6vnuONmsjbtBlpM=";
-    });
+    };
     date = "2022-04-10";
   };
   rime-wubi86-jidian = {
     pname = "rime-wubi86-jidian";
-    version = "b08f468ecc5318a8bf91c1df373cda3362719de4";
-    src = fetchFromGitHub ({
+    version = "f5185c6608a3afea9f84099d50c86ae74b948465";
+    src = fetchFromGitHub {
       owner = "KyleBing";
       repo = "rime-wubi86-jidian";
-      rev = "b08f468ecc5318a8bf91c1df373cda3362719de4";
+      rev = "f5185c6608a3afea9f84099d50c86ae74b948465";
       fetchSubmodules = false;
-      sha256 = "sha256-vdzQcqQ//kpocja0seeRNGTqnJg4jQNX2AXGDWl+FlA=";
-    });
-    date = "2023-07-01";
+      sha256 = "sha256-ze4yiaU3gDe1NaTWYC4DYk9HQpKkl2xdYFx/ECWm2Jo=";
+    };
+    date = "2023-08-25";
   };
   sddm-chili = {
     pname = "sddm-chili";
     version = "0.1.5";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "MarianArlt";
       repo = "sddm-chili";
       rev = "0.1.5";
       fetchSubmodules = false;
       sha256 = "sha256-wxWsdRGC59YzDcSopDRzxg8TfjjmA3LHrdWjepTuzgw=";
-    });
+    };
   };
   sddm-slice = {
     pname = "sddm-slice";
     version = "1.5.1";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "RadRussianRus";
       repo = "sddm-slice";
       rev = "1.5.1";
       fetchSubmodules = false;
       sha256 = "sha256-1AxRM2kHOzqjogYjFXqM2Zm8G3aUiRsdPDCYTxxQTyw=";
-    });
+    };
   };
   vonfry-icon = {
     pname = "vonfry-icon";
-    version = "2023-08-03";
+    version = "2023-08-26";
     src = fetchurl {
       url = "https://vonfry.name/images/default/logo-white.png";
       sha256 = "sha256-57Vf7OS5H6va5M5Tmx87FmpSZtIlqldeiBkwcbrrEm0=";
