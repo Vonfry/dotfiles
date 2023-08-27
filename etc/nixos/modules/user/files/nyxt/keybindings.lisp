793b06af1196580b000a6c8f1d85c72a21ba8cdf
Vonfry <mail@vonfry.name>
2023-08-27 12:44:32 +0800

    nyxt/keybindings: use emacs keybind in prompt buffer

--- etc/nixos/modules/user/files/nyxt/keybindings.lisp
+++ etc/nixos/modules/user/files/nyxt/keybindings.lisp
@@ -4,7 +4,7 @@
   ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
 (define-configuration prompt-buffer
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+  ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
 
 (defun play-video-at-url (url)
   (uiop:run-program (list "mpv" (render-url url))))
