7080f61498509646e29530f1bc182de3af950ef8
Vonfry <mail@vonfry.name>
2023-10-26 10:31:07 +0800

    emacs/comment: rebind these keys

--- etc/nixos/modules/user/files/emacs.d/modules/dev/nerd-commenter/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/dev/nerd-commenter/packages.el
@@ -2,14 +2,28 @@
 ;;
 
 (use-package evil-nerd-commenter
+  :after evil
   :general
   (nmap-leader :keymaps 'prog-mode-map
-    "c i"  'evilnc-comment-or-uncomment-lines
-    "c l"  'evilnc-quick-comment-or-uncomment-to-the-line
-    "c c"  'evilnc-copy-and-comment-lines
-    "c p"  'evilnc-comment-or-uncomment-paragraphs
-    "c v"  'evilnc-toggle-invert-comment-line-by-line
-    "c ."  'evilnc-copy-and-comment-operator
-    "c /"  'evilnc-comment-operator)
+    "c i" 'evilnc-comment-or-uncomment-lines
+    "c l" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "c c" 'evilnc-copy-and-comment-lines
+    "c p" 'evilnc-comment-or-uncomment-paragraphs
+    "c s" 'evilnc-comment-box
+    "c r" 'comment-or-uncomment-region
+    "c v" 'evilnc-toggle-invert-comment-line-by-line
+    "c ." 'evilnc-copy-and-comment-operator
+    "c ," 'evilnc-comment-operator)
+  (:keymaps 'evil-inner-text-objects-map
+             evilnc-comment-text-object
+             'evilnc-inner-commenter)
+  (:keymaps 'evil-outer-text-objects-map
+             evilnc-comment-text-object
+             'evilnc-outer-commenter)
   (vmap-leader :keymaps 'prog-mode-map
-    "c r" 'comment-or-uncomment-region))
+    "c i" 'evilnc-comment-or-uncomment-lines
+    "c c" 'evilnc-copy-and-comment-lines
+    "c s" 'evilnc-comment-box
+    "c r" 'comment-or-uncomment-region
+    "c ." 'evilnc-copy-and-comment-operator
+    "c ," 'evilnc-comment-operator))
