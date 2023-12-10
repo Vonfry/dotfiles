ced1197b4f4e665a62d21ec3d7b395b79f6ad811
Vonfry <mail@vonfry.name>
2023-12-10 20:13:24 +0800

    home-manager/mimetypes: remove browser because nyxt has one now

--- etc/nixos/modules/user/net.nix
+++ etc/nixos/modules/user/net.nix
@@ -8,15 +8,6 @@ let
   colorscheme = config.lib.theme.dracula;
 
   whether_emacsclient_email = config.services.emacs.enable && cfg.email != null;
-
-  browser_mimetypes = ["x-scheme-handler/http" "x-scheme-handler/https"
-                       "x-scheme-handler/ftp" "x-scheme-handler/chrome"
-                       "text/html" "text/xml" "application/x-extension-htm"
-                       "application/x-extension-html"
-                       "application/x-extension-shtml" "application/xhtml+xml"
-                       "application/x-extension-xhtml"
-                       "application/x-extension-xht"
-                      ];
 in {
   options.vonfry.net = {
     email = mkOption {
@@ -91,7 +82,6 @@ in {
 
     xdg = {
       mimeApps.defaultApplications = mkMerge [
-        (genAttrs browser_mimetypes (const "nyxt.desktop"))
         (mkIf whether_emacsclient_email {
           "x-scheme-handler/mailto" = "emacsclient-mail.desktop";
         })
