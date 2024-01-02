185ed12007f9de3bb6f9eacead5d960cf742affa
Vonfry <mail@vonfry.name>
2024-01-02 18:53:31 +0800

    use add-to-list instead of custom to override major-mode-remap-alist in emacs

--- etc/nixos/modules/user/files/emacs.d/modules/dev/tree-sisster/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/dev/tree-sisster/packages.el
@@ -2,21 +2,23 @@
 ;;
 
 (use-package treesit
-  :custom
-  (major-mode-remap-alist
-   '(; (haskell-mode . haskell-ts-mode)
-     (c-mode . c-ts-mode)
-     (c++-mode . c++-ts-mode)
-     (rust-mode . rust-ts-mode)
-     ; (nix-mode . nix-ts-mode)
-     ; (latex-mode . latex-ts-mode)
-     ; (bibtex-mode . bibtex-ts-mode)
-     ; (elisp-mode . elisp-ts-mode)
-     ; (lisp-mode . lisp-ts-mode)
-     ; (julia-mode . julia-ts-mode)
-     ; (perl-mode . perl-ts-mode)
-     ; (cperl-mode . cperl-ts-mode)
-     (yaml-mode . yaml-ts-mode)
-     (toml-mode . toml-ts-mode)
-     (json-mode . json-ts-mode)
-     (html-mode . html-ts-mode))))
+  :config
+  (let ((remap-alist
+         '(; (haskell-mode . haskell-ts-mode)
+           (c-mode . c-ts-mode)
+           (c++-mode . c++-ts-mode)
+           (rust-mode . rust-ts-mode)
+           ; (nix-mode . nix-ts-mode)
+           ; (latex-mode . latex-ts-mode)
+           ; (bibtex-mode . bibtex-ts-mode)
+           ; (elisp-mode . elisp-ts-mode)
+           ; (lisp-mode . lisp-ts-mode)
+           ; (julia-mode . julia-ts-mode)
+           (yaml-mode . yaml-ts-mode)
+           (toml-mode . toml-ts-mode)
+           (json-mode . json-ts-mode)
+           (html-mode . html-ts-mode))))
+    (mapc (lambda (alist)
+            (add-to-list 'major-mode-remap-alist alist))
+          remap-alist)))
+
