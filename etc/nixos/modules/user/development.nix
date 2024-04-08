83e323c3ccc92dc504fa7ae03bbeb85458baf430
Vonfry <mail@vonfry.name>
2024-04-08 19:34:28 +0800

    readd direnv stdlib to change che cache dir

--- etc/nixos/modules/user/development.nix
+++ etc/nixos/modules/user/development.nix
@@ -239,9 +239,16 @@ in {
 
       direnv = {
         enable = true;
-        nix-direnv = {
-          enable = true;
-        };
+        nix-direnv.enable = true;
+        stdlib = ''
+          declare -A direnv_layout_dirs
+          direnv_layout_dir() {
+              echo "''${direnv_layout_dirs[$PWD]:=$(
+                  echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+                  echo -n "$PWD" | shasum | cut -d ' ' -f 1
+              )}"
+          }
+        '';
       };
     };
 
