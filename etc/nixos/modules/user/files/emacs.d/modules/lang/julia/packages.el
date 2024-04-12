9c47f25118dfc58a3fd40ab215aed701cdd62698
Vonfry <moooffice@vonfry.name>
2024-04-12 14:10:27 +0800

    update julia repl keybindings

--- etc/nixos/modules/user/files/emacs.d/modules/lang/julia/packages.el
+++ etc/nixos/modules/user/files/emacs.d/modules/lang/julia/packages.el
@@ -9,20 +9,19 @@
   :general
   (nmap-leader :keymaps 'julia-mode-map
     "\"" 'julia-repl)
-  (nmap-mode :keymaps 'julia-repl-mode-map
-    "a" 'julia-repl-activate-parent
-    "b" 'julia-repl-send-buffer
-    "c" 'julia-repl-send-region-or-line
-    "d" 'julia-repl-doc
-    "e" 'julia-repl-edit
-    "l" 'julia-repl-list-methods
-    "m" 'julia-repl-macroexpand
-    "p" 'julia-repl-cd
-    "s" 'julia-repl-prompt-set-inferior-buffer-name-suffix
-    "t" 'julia-repl-includet-buffer
-    "v" 'julia-repl-prompt-set-executable-key
-    "z" 'julia-repl
-    "C-<return>" 'julia-repl-send-line))
+  (nmap-leader :keymaps 'julia-repl-mode-map
+    "' a" 'julia-repl-activate-parent
+    "' b" 'julia-repl-send-buffer
+    "' c" 'julia-repl-send-region-or-line
+    "' d" 'julia-repl-doc
+    "' e" 'julia-repl-edit
+    "' l" 'julia-repl-list-methods
+    "' m" 'julia-repl-macroexpand
+    "' p" 'julia-repl-cd
+    "' s" 'julia-repl-prompt-set-inferior-buffer-name-suffix
+    "' t" 'julia-repl-includet-buffer
+    "' v" 'julia-repl-prompt-set-executable-key
+    "' RET" 'julia-repl-send-line))
 
 (use-package eglot-jl
   :hook (julia-mode . eglot-ensure))
