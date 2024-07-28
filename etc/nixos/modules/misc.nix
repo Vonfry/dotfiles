50561834bf4451969cd1b5002b844ff34340c8b6
Vonfry <mail@vonfry.name>
2024-07-28 12:43:44 +0800

    remove unnecessary sound option

--- etc/nixos/modules/misc.nix
+++ etc/nixos/modules/misc.nix
@@ -5,7 +5,6 @@ let
   cfg = config.vonfry;
 in {
   config = mkIf cfg.enable {
-    sound.enable = mkDefault (!cfg.workspace.server);
     services.pipewire = {
       enable = mkDefault (!cfg.workspace.server);
       alsa.enable = true;
