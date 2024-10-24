0f9f899cac59184b7d03227d40112066947c846e
Vonfry <mail@vonfry.name>
2024-10-24 16:16:48 +0800

    update nyxt stylesheet

--- etc/nixos/modules/user/files/nyxt/stylesheet.lisp
+++ etc/nixos/modules/user/files/nyxt/stylesheet.lisp
@@ -3,21 +3,43 @@
 (define-configuration browser
     ((theme
       (make-instance 'theme:theme
+                     ; take from daiysui dracula theme.
+                     ; constrast color is mixed with black or white.
                      :monospace-font-family "monospace"
                      :font-family "sans serif"
-                     :background-color+ "#6272a4"
                      :background-color  "#282a36"
-                     :background-color- "#44475a"
-                     :on-background-color- "#f8f8f2"
-                     :on-background-color  "#f8f8f2"
-                     :on-background-color+ "#f8f8f2"
-                     :action-color "#f1fa8c"
-                     :action-color+ "#f1fa8c"
-                     :action-color- "#f1fa8c"
-                     :on-action-color "#282a36"
-                     :on-action-color+ "#282a36"
-                     :on-action-color- "#282a36"
-                     :primary-color "#bd93f9"
-                     :on-primary-color "#282a36"
-                     :secondary-color "#ff79c6"
-                     :on-secondary-color "#282a36"))))
+                     :background-color+ "#3e3f4a"
+                     :background-color- "#3e3f4a"
+                     :on-background-color  nil
+                     :primary-color "#ff79c6"
+                     :primary-color+ "#e66db2"
+                     :primary-color- "#e66db2"
+                     :on-primary-color "#16050e"
+                     :secondary-color "#bd93f9"
+                     :secondary-color+ "#aa84e0"
+                     :secondary-color- "#aa84e0"
+                     :on-secondary-color "#16050e"
+                     :action-color "#ffb86c"
+                     :action-color+ "#e6a661"
+                     :action-color- "#e6a661"
+                     :on-action-color "#16050e"
+                     :highlight-color "#44475a"
+                     :highlight-color+ "#57596a"
+                     :highlight-color- "#57596a"
+                     :on-highlight-color nil
+                     :success-color "#50fa7b"
+                     :success-color+ "#48e16f"
+                     :success-color- "#48e16f"
+                     :on-success-color "#16050e"
+                     :warning-color "#f1fa8c"
+                     :warning-color+ "#d9e17e"
+                     :warning-color- "#d9e17e"
+                     :on-warning-color "#16050e"
+                     :codeblock-color "#000000"
+                     :codeblock-color+ "#6272a4"
+                     :codeblock-color- "#6272a4"
+                     :on-codeblock-color nil
+                     :text-color "#f8f8f2"
+                     :text-color+ "#f8f8f2"
+                     :text-color- "#f8f8f2"
+                     :contrast-text-color "#6272a4"))))
