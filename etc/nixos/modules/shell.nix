fbee856945a436706583a7a189e9b9b0940f8cdf
Vonfry <mail@vonfry.name>
2024-11-27 19:38:52 +0800

    enable nixos fish module

--- etc/nixos/modules/shell.nix
+++ etc/nixos/modules/shell.nix
@@ -13,6 +13,9 @@ in
   config = mkIf cfg.enable {
     programs.command-not-found.enable = false;
 
+    # Let vendors work in hm.
+    programs.fish.enable = true;
+
     console = {
       font = "ter-v12n";
       packages = with pkgs; [ terminus_font ];
