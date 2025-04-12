9998e175dee0676a91698412b6f9187d40391595
Vonfry <dev@vonfry.name>
2025-04-12 12:01:05 +0800

    add nix eshell to globally mapping

--- etc/nixos/modules/home/files/emacs.d/modules/lang/nix/packages.el
+++ etc/nixos/modules/home/files/emacs.d/modules/lang/nix/packages.el
@@ -5,6 +5,7 @@
   :interpreter ("nix-shell" . nix-shebang-mode)
   :hook ((nix-mode nix-ts-mode) . eglot-ensure)
   :general
+  (nmap-leader "' $" 'nix-eshell)
   (nmap-leader :keymaps '(nix-mode-map nix-ts-mode-map)
     "="   'nix-format-buffer
     "\""  'nix-repl
