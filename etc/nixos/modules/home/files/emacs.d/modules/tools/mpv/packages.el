ac52f351374b9dd16f9e5712c768c3eb6211f799
Vonfry <dev@vonfry.name>
2025-05-18 14:03:04 +0800

    add some empv custom

--- etc/nixos/modules/home/files/emacs.d/modules/tools/mpv/packages.el
+++ etc/nixos/modules/home/files/emacs.d/modules/tools/mpv/packages.el
@@ -3,6 +3,12 @@
 
 (use-package empv
   :after embark
+  :custom
+  (empv-mpv-args '("--no-terminal"
+                   "--idle"
+                   "--input-ipc-server=/tmp/empv-socket"))
+  (empv-video-dir (list (xdg-user-dir "VIDEOS")))
+  (empv-audio-dir (list (xdg-user-dir "MUSIC")))
   :config
   (empv-embark-initialize-extra-actions)
   :general
