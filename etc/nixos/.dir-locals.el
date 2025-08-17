558d93ffeae2b1487083bd084d64924beda395dc
Vonfry <dev@vonfry.name>
2025-08-17 12:05:19 +0800

    fix dir locals on eglot set

--- etc/nixos/.dir-locals.el
+++ etc/nixos/.dir-locals.el
@@ -2,18 +2,20 @@
                        (let ((getflake (lambda (s)
                                          (format
                                           "(builtins.getFlake \"path:%s\").%s"
-                                          (dir-locals-find-file ".")))))
+                                          (dir-locals-find-file ".")
+                                          s))))
                          (setq-local
                           eglot-workspace-configuration
                           `(:nixd
                             (:nixpkgs
-                             (:expr (format
-                                     "import %s { }"
-                                     (getflake "inputs.nixpkgs")))
+                             (:expr ,(format
+                                      "import %s { }"
+                                      (funcall getflake "inputs.nixpkgs")))
                              :formatting
                              (:command ["nixfmt"])
                              :options
                              (:nixos
                               (:expr
-                               (getflake
-                                "nixosConfigurations.vonfry.options")))))))))))
+                               ,(funcall
+                                 getflake
+                                 "nixosConfigurations.vonfry.options")))))))))))
