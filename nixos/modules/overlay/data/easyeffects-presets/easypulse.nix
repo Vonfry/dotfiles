e60e0d789cc096902d3d743a1b03bfd01f5fb234
Vonfry <dev@vonfry.name>
2025-08-23 14:09:12 +0800

    fix easypulse

--- nixos/modules/overlay/data/easyeffects-presets/easypulse.nix
+++ nixos/modules/overlay/data/easyeffects-presets/easypulse.nix
@@ -7,5 +7,5 @@ let
   src = sources.easypulse.src;
 in
 runCommandNoCC "easypulse" { } ''
-  install -D -t $out/share/easyeffects/output/easypulse ${src}/output/*.json
+  install -D -t $out/share/easyeffects/output/easypulse ${src}/p-chan5/*.json
 ''
